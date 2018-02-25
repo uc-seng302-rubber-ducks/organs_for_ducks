e195b89b5a3d10c090337a27afd1ba7d323ea3ee
Josh Burt <jbu71@uclive.ac.nz>
2018-02-26 10:50:45 +1300

    Added Json writer and directory creation
    -needs directory path structure to be full implemented
    
    #pair[jbu71, tke34]
    #story 66

--- src/main/java/seng302/App.java
+++ src/main/java/seng302/App.java
@@ -1,14 +1,24 @@
 package seng302;
 
-import org.joda.time.DateTime;
 
+import java.io.IOException;
+
+import java.util.ArrayList;
 import java.util.Date;
 
 public class App
 {
-    public static void main( String[] args )
-    {
-        Donor d = new Donor("bob", new Date(1,1,2018));
+    public static void main( String[] args ) {
+        Donor d = new Donor("bob", new Date(1, 1, 2018));
         System.out.println(d);
+        ArrayList<Donor> ds = new ArrayList<Donor>();
+        ds.add(d);
+
+        try {
+            JsonWriter.saveCurrentDonorState(ds);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
     }
 }
---
+++ src/main/java/seng302/JsonWriter.java
@@ -0,0 +1,42 @@
+package seng302;
+
+import org.json.simple.JSONObject;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public final class JsonWriter {
+
+
+    public static void saveCurrentDonorState(ArrayList<Donor> donors) throws IOException {
+        Files.createDirectories(Paths.get(String.valueOf(Directory.JSON)));
+        System.out.println("Created Directories:" + String.valueOf(Directory.JSON));
+        File outFile = new File(Directory.JSON+"donors.json"); //#TODO: change to absolute file paths and create hidden folder for user data
+        if (outFile.exists()){
+            outFile.delete(); //purge old data before writing new data in
+        }
+
+        outFile.createNewFile(); //creates new file if donors does not exist
+        FileOutputStream outFileStream = new FileOutputStream(outFile, true);
+
+        for (Donor d : donors){
+            JSONObject j = new JSONObject();
+            j.put("name", d.getName());
+            j.put("DOB", d.getDateOfBirth());
+            j.put("DOD", d.getDateOfDeath());
+            j.put("Gender", d.getGender());
+            j.put("Height", d.getHeight());
+            j.put("Weight", d.getWeight());
+            j.put("Blood Type", d.getBloodType());
+            j.put("Current Address", d.getCurrentAddress());
+            j.put("Region", d.getRegion());
+            j.put("Time Created", d.getTimeCreated());
+            outFileStream.write(j.toJSONString().getBytes());
+        }
+        outFileStream.close();
+    }
+}
