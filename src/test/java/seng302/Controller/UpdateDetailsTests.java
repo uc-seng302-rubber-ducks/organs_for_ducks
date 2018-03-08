d18da376700fe9dbbd3c59d6905e748645457d09
Tyler Kennedy <tyler.kennedy@jupl.com>
2018-03-08 13:54:47 +1300

    finished update details tests #pair[jbu71, tke34] #story 67

--- src/test/java/seng302/Controller/UpdateDetailsTests.java
+++ src/test/java/seng302/Controller/UpdateDetailsTests.java
@@ -5,49 +5,149 @@ import static org.junit.Assert.fail;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import org.joda.time.DateTime;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import picocli.CommandLine;
-import seng302.Controller.CliCommands.Register;
-import seng302.Controller.CliCommands.Update;
+
+import seng302.Controller.CliCommands.UpdateDetails;
+import seng302.Controller.CliCommands.UpdateRemoveOrgans;
 import seng302.Model.Donor;
-import seng302.Model.Organs;
 
 public class UpdateDetailsTests {
 
+  AppController controller;
+  SimpleDateFormat sdf;
+  int id = -1;
+  @Before
+  public void resetDonor() {
+    sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+    controller = AppController.getInstance();
+    controller.setDonors(new ArrayList<>());
+
+    try {
+      id = controller.Register("test dummy", sdf.parse("1111-11-11"));
+      Donor donor = controller.findDonors("test dummy").get(0);
+      donor.setWeight(65.3);
+    }
+    catch (Exception ex) {
+      fail("Exception thrown setting up tests");
+    }
+  }
 
   @Test
   public void ShouldUpdateFirstName() {
-    fail("not yet implemented");
+    String[] args = {"-id="+id, "-f=Mal"};
+    new CommandLine(new UpdateDetails())
+        .parseWithHandler(new CommandLine.RunLast(), System.err, args);
+
+    Donor mal = controller.findDonors("Mal dummy").get(0);
+    Assert.assertNotNull(mal);
+
+    ArrayList<Donor> test = controller.findDonors("test dummy");
+    assert(test.size() == 0);
   }
 
   @Test
   public void ShouldUpdateLastName() {
-    fail("not yet implemented");
+    String[] args = {"-id="+id, "-l=muppet"};
+    new CommandLine(new UpdateDetails())
+        .parseWithHandler(new CommandLine.RunLast(), System.err, args);
+
+    Donor muppet = controller.findDonors("test muppet").get(0);
+    Assert.assertNotNull(muppet);
+
+    ArrayList<Donor> test = controller.findDonors("test dummy");
+    assert(test.size() == 0);
   }
 
   @Test
   public void ShouldUpdateFullName() {
-    fail("not yet implemented");
+    String[] args = {"-id="+id, "-f=stephen", "-l=hawking"};
+    new CommandLine(new UpdateDetails())
+        .parseWithHandler(new CommandLine.RunLast(), System.err, args);
+
+    Donor alan = controller.findDonors("stephen hawking").get(0);
+    Assert.assertNotNull(alan);
+
+    ArrayList<Donor> test = controller.findDonors("test dummy");
+    assert(test.size() == 0);
   }
 
   @Test
   public void ShouldUpdateNumberField() {
     //height and weight are identical, no use testing both
     //just checking it can parse numbers
-    fail("not yet implemented");
+    String[] args = {"-id="+id, "-w=100"};
+    new CommandLine(new UpdateDetails())
+        .parseWithHandler(new CommandLine.RunLast(), System.err, args);
+
+    Donor test = controller.findDonors("test dummy").get(0);
+    assert(test.getWeight() == 100);
+  }
+
+  @Test
+  public void ShouldNotUpdateBadNumberField() {
+    //height and weight are identical, no use testing both
+    String[] args = {"-id="+id, "-w=fat"};
+    new CommandLine(new UpdateDetails())
+        .parseWithHandler(new CommandLine.RunLast(), System.err, args);
+
+    Donor test = controller.findDonors("test dummy").get(0);
+    assert(test.getWeight() == 65.3);
   }
 
   @Test
   public void ShouldUpdateDateField() {
     //dob and dod are identical, no use testing both
     //just checking it can parse dates
-    fail("not yet implemented");
+    String[] args = {"-id="+id, "-dob=2020-3-4"};
+
+    new CommandLine(new UpdateDetails())
+        .parseWithHandler(new CommandLine.RunLast(), System.err, args);
+
+    Donor test = controller.findDonors("test dummy").get(0);
+    try {
+      assert (test.getDateOfBirth().equals(sdf.parse("2020-3-4")));
+    }
+    catch (ParseException ex) {
+      fail("Could not parse date (error in tester)");
+    }
+  }
+
+  @Test
+  public void ShouldNotUpdateBadDate() {
+    //dob and dod are identical, no use testing both
+    String[] args = {"-id="+id, "-dob=1963"};
+
+    new CommandLine(new UpdateDetails())
+        .parseWithHandler(new CommandLine.RunLast(), System.err, args);
+
+    Donor test = controller.findDonors("test dummy").get(0);
+    try {
+      assert (test.getDateOfBirth().equals(sdf.parse("1111-11-11")));
+    }
+    catch (ParseException ex) {
+      fail("Could not parse date (error in tester)");
+    }
+
   }
 
   @Test
   public void ShouldUpdateLastModifiedTimestamp() {
-    fail("not yet implemented");
+    Donor donor = controller.getDonor(id);
+    DateTime oldTime = donor.getLastModified();
+
+    String[] args = {"-id="+id, "-f=fred"};
+
+    new CommandLine(new UpdateDetails())
+        .parseWithHandler(new CommandLine.RunLast(), System.err, args);
+
+    DateTime newTime = donor.getLastModified();
+    assert(newTime.isAfter(oldTime));
   }
 }
