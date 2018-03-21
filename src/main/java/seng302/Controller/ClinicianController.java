9979e0a4449e45cfad07fbb7a4863871f1e7c580
Tyler Kennedy <tke34@uclive.ac.nz>
2018-03-21 18:53:25 +1300

    implement full filtering/sorting functionality with few table columns. More columns to be added as needed #story[404]

--- src/main/java/seng302/Controller/ClinicianController.java
+++ src/main/java/seng302/Controller/ClinicianController.java
@@ -6,17 +6,21 @@ import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Pagination;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import org.joda.time.DateTime;
 import seng302.Controller.AppController;
@@ -27,114 +31,173 @@ import seng302.Model.Donor;
 
 public class ClinicianController {
 
-    @FXML
-    private TextField regionTextField;
+  private final int ROWS_PER_PAGE = 30;
+  @FXML
+  private TextField regionTextField;
 
-    @FXML
-    private TextField addressTextField;
+  @FXML
+  private TextField addressTextField;
 
-    @FXML
-    private PasswordField conformPasswordField;
+  @FXML
+  private PasswordField conformPasswordField;
 
-    @FXML
-    private TextField nameTextField;
+  @FXML
+  private TextField nameTextField;
 
-    @FXML
-    private Button logoutButton;
+  @FXML
+  private Button logoutButton;
 
-    @FXML
-    private PasswordField passwordField;
+  @FXML
+  private PasswordField passwordField;
 
-    @FXML
-    private Button confirmButton;
+  @FXML
+  private Button confirmButton;
 
-    @FXML
-    private Button undoButton;
+  @FXML
+  private Button undoButton;
 
-    @FXML
-    private Label staffIdLabel;
+  @FXML
+  private Label staffIdLabel;
 
-    @FXML
-    private Label warningLabel;
+  @FXML
+  private Label warningLabel;
 
-    @FXML
-    private TextField searchTextField;
+  @FXML
+  private TextField searchTextField;
 
-    @FXML
-    private TableView<Donor> searchTableView;
+  @FXML
+  private TableView<Donor> searchTableView;
 
-    private Stage stage;
-    private AppController appController;
-    private Clinician clinician;
-    private ArrayList<Donor> donors;
+  @FXML
+  private Pagination searchPaginationControl;
+
+  private Stage stage;
+  private AppController appController;
+  private Clinician clinician;
+  private ArrayList<Donor> donors;
 
 
-    public void init(Stage stage, AppController appController, Clinician clinician){
-        warningLabel.setText("");
-        this.stage = stage;
-        this.appController = appController;
-        this.clinician = clinician;
-        nameTextField.setText(clinician.getName());
-        staffIdLabel.setText(String.valueOf(clinician.getStaffId()));
-        addressTextField.setText(clinician.getWorkAddress());
-        regionTextField.setText(clinician.getRegion());
-        donors = appController.getDonors();
-        initSearchTable();
+  public void init(Stage stage, AppController appController, Clinician clinician) {
+    warningLabel.setText("");
+    this.stage = stage;
+    this.appController = appController;
+    this.clinician = clinician;
+    nameTextField.setText(clinician.getName());
+    staffIdLabel.setText(String.valueOf(clinician.getStaffId()));
+    addressTextField.setText(clinician.getWorkAddress());
+    regionTextField.setText(clinician.getRegion());
+    donors = appController.getDonors();
+    initSearchTable();
 
+  }
+
+
+  /**
+   * initialises the search table, abstracted from main init function for clarity
+   */
+  private void initSearchTable() {
+    if (donors == null || donors.isEmpty()) {
+      return;
     }
+    //set up lists
+    //table contents are SortedList of a FilteredList of an ObservableList of an ArrayList
+    ObservableList<Donor> oListDonors = FXCollections.observableArrayList(donors);
 
-    private void initSearchTable() {
-        if (donors == null || donors.isEmpty()) {
-            return;
+    TableColumn<Donor, String> nameColumn = new TableColumn<>("Name");
+    nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+    TableColumn<Donor, Integer> dobColumn = new TableColumn<>("Date of Birth");
+    dobColumn.setCellValueFactory(new PropertyValueFactory<>("dateOfBirth"));
+
+    TableColumn<Donor, Integer> dodColumn = new TableColumn<>("Date of Death");
+    dodColumn.setCellValueFactory(new PropertyValueFactory<>("dateOfDeath"));
+
+    //TODO add more columns as wanted/needed
+    FilteredList<Donor> fListDonors = new FilteredList<>(oListDonors);
+    fListDonors = filter(searchTextField, fListDonors);
+
+    SortedList<Donor> sListDonors = new SortedList<>(fListDonors);
+    sListDonors.comparatorProperty().bind(searchTableView.comparatorProperty());
+
+    //set table columns and contents
+    searchTableView.getColumns().setAll(nameColumn, dobColumn, dodColumn);
+
+    searchPaginationControl = new Pagination((fListDonors.size() / ROWS_PER_PAGE + 1), 0);
+    //searchPaginationControl.setPageFactory(this::);
+
+    searchTableView.setItems(sListDonors);
+
+
+    //set on-click behaviour
+    searchTableView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+      @Override
+      public void handle(MouseEvent event) {
+        if (event.isPrimaryButtonDown()) {
+
         }
-        ObservableList<Donor> oListDonors =FXCollections.observableArrayList(donors);
+      }
+    });
 
-        TableColumn<Donor, String> nameColumn = new TableColumn<>("Name");
-        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+  }
 
-        FilteredList<Donor> fListDonors = new FilteredList<>(oListDonors);
-
-        SortedList<Donor> sListDonors = new SortedList<>(fListDonors);
-
-        searchTableView.getColumns().setAll(nameColumn);
-        searchTableView.setItems(sListDonors);
-
-
-    }
-    @FXML
-    void undo(ActionEvent event) {
-
-    }
-
-    @FXML
-    void logout(ActionEvent event) {
-        confirm(new ActionEvent());
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/loginView.fxml"));
-        Parent root = null;
-        try {
-            root = loader.load();
-        } catch (IOException e) {
-            e.printStackTrace();
+  /**
+   *  applies a filter to a filtered list
+   * @param inputTextField text field from which the list will be filtered
+   * @param fListDonors list to be filtered
+   * @return filtered list with filter applied
+   */
+  private static FilteredList<Donor> filter(TextField inputTextField, FilteredList<Donor> fListDonors ) {
+    inputTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+      fListDonors.setPredicate(Donor -> {
+        if (newValue == null || newValue.isEmpty()) {
+          return true;
         }
-        LoginController loginController = loader.getController();
-        loginController.init(AppController.getInstance(), stage);
-        stage.setScene(new Scene(root));
-        stage.show();
-    }
-
-    @FXML
-    void confirm(ActionEvent event) {
-        clinician.setName(nameTextField.getText());
-        clinician.setWorkAddress(addressTextField.getText());
-        clinician.setRegion(regionTextField.getText());
-        if(passwordField.getText().equals(conformPasswordField.getText()) && !passwordField.getText().equals("")){
-            clinician.setPassword(passwordField.getText());
-        } else {
-            warningLabel.setText("Passwords did not match.\n Password was not updated.");
+        String lowerCaseFilterText = newValue.toLowerCase();
+        if (Donor.getName().contains(lowerCaseFilterText)) {
+          return true;
         }
-        clinician.setDateLastModified(DateTime.now());
-        appController.updateClinicians(clinician);
+        //if (other test case) return true
+        return false;
+      });
+    });
+    return fListDonors;
+  }
 
+  @FXML
+  void undo(ActionEvent event) {
+
+  }
+
+  @FXML
+  void logout(ActionEvent event) {
+    confirm(new ActionEvent());
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/loginView.fxml"));
+    Parent root = null;
+    try {
+      root = loader.load();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+    LoginController loginController = loader.getController();
+    loginController.init(AppController.getInstance(), stage);
+    stage.setScene(new Scene(root));
+    stage.show();
+  }
+
+  @FXML
+  void confirm(ActionEvent event) {
+    clinician.setName(nameTextField.getText());
+    clinician.setWorkAddress(addressTextField.getText());
+    clinician.setRegion(regionTextField.getText());
+    if (passwordField.getText().equals(conformPasswordField.getText()) && !passwordField.getText()
+        .equals("")) {
+      clinician.setPassword(passwordField.getText());
+    } else {
+      warningLabel.setText("Passwords did not match.\n Password was not updated.");
+    }
+    clinician.setDateLastModified(DateTime.now());
+    appController.updateClinicians(clinician);
+
+  }
 
 }
