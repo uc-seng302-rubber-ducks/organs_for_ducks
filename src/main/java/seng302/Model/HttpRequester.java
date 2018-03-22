f6fd11c60f5868565ee4b939bc714cc24d764159
Tyler Kennedy <tke34@uclive.ac.nz>
2018-03-22 16:06:30 +1300

    added skeleton filtered response #story[398]

--- src/main/java/seng302/Model/HttpRequester.java
+++ src/main/java/seng302/Model/HttpRequester.java
@@ -5,6 +5,10 @@ import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class HttpRequester {
 
@@ -21,13 +25,34 @@ public class HttpRequester {
     String url = "https://www.ehealthme.com/api/v1/drug-interaction/"+drugOneName+"/"+drugTwoName+"/";
     Request request = new Request.Builder().url(url).build();
     Response response = client.newCall(request).execute();
-    return response.body().string();
+    try {
+      return response.body().string();
+    } catch (NullPointerException ex) {
+      return "";
+    }
+  }
+  public static String getDrugInteractions(String drugOneName, String drugTwoName, String gender, int Age) throws IOException {
+
+    OkHttpClient client = new OkHttpClient();
+    String url = "https://www.ehealthme.com/api/v1/drug-interaction/"+drugOneName+"/"+drugTwoName+"/";
+    Request request = new Request.Builder().url(url).build();
+    Response response = client.newCall(request).execute();
+
+    try {
+      String rawString = response.body().string();
+      JSONParser parser = new JSONParser();
+      JSONObject json = (JSONObject) parser.parse(rawString);
+      JSONArray ageInteractions = (JSONArray) json.get("age_interaction");
+    } catch (Exception ex) {
+      return "";
+    }
+    return ""; //TODO change this
   }
 
   public static  void main(String[] args) {
     System.out.println("Please don't run me, this is for testing only");
     try {
-      String res = getDrugInteractions("panadol", "tramadol");
+      String res = getDrugInteractions("tramadol", "panadol");
       System.out.println(res);
     }
     catch (Exception ex) {
