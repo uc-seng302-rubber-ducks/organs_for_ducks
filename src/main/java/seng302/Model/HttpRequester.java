1b2d8b41d434c9ddcf95cf6e705a8d28e26af0b6
Tyler Kennedy <tke34@uclive.ac.nz>
2018-04-09 13:23:14 +1200

    added duration to the output strings as per AC3. - duration is the first one in a list, not necessarily highest or lowest. tries to avoid "not specified" when possible #story[588]

--- src/main/java/seng302/Model/HttpRequester.java
+++ src/main/java/seng302/Model/HttpRequester.java
@@ -1,5 +1,7 @@
 package seng302.Model;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -18,6 +20,7 @@ public class HttpRequester {
    * uses ehealthme api to get interactions between two drugs
    * @param drugOneName string name of first drug
    * @param drugTwoName string name of second drug
+   * @param client http client to be used. use `new OkHttpClient()` if you have no preference
    * @return json formatted string containing the interactions between the two drugs
    * @throws IOException caused by error with server connection
    */
@@ -34,11 +37,22 @@ public class HttpRequester {
   }
 
 
+  /**
+   * returns a set containing strings of format interaction (duration)
+   * @param drugOneName name of the first drug
+   * @param drugTwoName name of the second drug
+   * @param gender gender of the user (anything starting with m or f will be taken as male or female)
+   * @param age integer age of the user
+   * @param client http client to be used. use `new OkHttpClient()` if you have no preference
+   * @return set containing strings of format interaction (duration). e.g. pneumonia (5 - 10 years)
+   * @throws IOException caused by error with server connection
+   */
   public static Set<String> getDrugInteractions(String drugOneName, String drugTwoName, String gender, int age, OkHttpClient client) throws IOException {
 
     Set<String> results = new HashSet<>();
     Set<String> ageResults = new HashSet<>();
     Set<String> genderResults = new HashSet<>();
+    Set<String> finalResults = new HashSet<>();
     String url = "https://www.ehealthme.com/api/v1/drug-interaction/"+drugOneName+"/"+drugTwoName+"/";
     Request request = new Request.Builder().url(url).build();
     Response response = client.newCall(request).execute();
@@ -61,6 +75,7 @@ public class HttpRequester {
       JSONObject json = (JSONObject) parser.parse(rawString);
       JSONObject ageInteractions = (JSONObject) json.get("age_interaction");
       JSONObject genderInteractions = (JSONObject) json.get("gender_interaction");
+      JSONObject durationInteractions = (JSONObject) json.get("duration_interaction");
       JSONArray ageProblems = (JSONArray) ageInteractions.get(ageRange);
       ageResults.addAll(ageProblems);
 
@@ -77,7 +92,41 @@ public class HttpRequester {
         genderResults.addAll(genderedInteractions);
       }
       ageResults.retainAll(genderResults);
-      return ageResults;
+      //cross current result set with durations to get symptom (duration) strings
+      Set<String> durationKeySet = durationInteractions.keySet();
+      HashMap<String, ArrayList<String>> temp = new HashMap<>();
+      for(String duration: durationKeySet) {
+        JSONArray afflictions = (JSONArray) durationInteractions.get(duration);
+        for (Object affliction: afflictions) {
+          String symptom = (String) affliction;
+          if (ageResults.contains(symptom)) {
+            //put into map
+            if (temp.containsKey(symptom)) {
+              ArrayList<String> newDurations = temp.get(symptom);
+              newDurations.add(duration);
+              temp.put(symptom, newDurations);
+            }
+            else {
+              ArrayList<String> newDurations = new ArrayList<>();
+              newDurations.add(duration);
+              temp.put(symptom, newDurations);
+            }
+          }
+        }
+      }
+      //cull duplicate symptoms with different duration entries
+      //from temp
+      Set<String> finalKeySet = temp.keySet();
+      for (String key: finalKeySet) {
+        //TODO currently gets first duration in array, not necessarily the shortest/longest
+        String duration = temp.get(key).get(0);
+        if (duration.equals("not specified") && temp.get(key).size() > 1) {
+          duration = temp.get(key).get(1);
+        }
+        finalResults.add(key+" ("+duration+")");
+      }
+
+      return finalResults;
     } catch (Exception ex) {
       return new HashSet<>();
     }
@@ -97,7 +146,7 @@ public class HttpRequester {
     System.out.println("Please don't run me, this is for testing only");
     try {
       //String res = getDrugInteractions("coumadin", "Acetaminophen", new OkHttpClient());
-      Set<String> res = getDrugInteractions("coumadin", "Acetaminophen","male",36, new OkHttpClient());
+      Set<String> res = getDrugInteractions("coumadin", "Acetaminophen","m",35, new OkHttpClient());
       System.out.println(res);
     }
     catch (Exception ex) {
