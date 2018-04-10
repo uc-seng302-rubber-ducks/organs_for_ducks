816388f7429758516d8f7c0c5e9e05883588a57d
Josh Burt <jbu71@uclive.ac.nz>
2018-04-10 16:07:56 +1200

    Added medical procedures class
    #story[605]

---
+++ src/main/java/seng302/Model/MedicalProcedure.java
@@ -0,0 +1,65 @@
+package seng302.Model;
+
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+/**
+ * Class for instantiating a medical procedure
+ * @author Josh Burt
+ */
+public class MedicalProcedure {
+
+    private LocalDate procedureDate;
+    private String summary;
+    private String description;
+    private ArrayList<Organs> organsAffected;
+
+    public MedicalProcedure(LocalDate procedureDate, String summary, String description, ArrayList<Organs> organsAffected) {
+
+        this.procedureDate = procedureDate;
+        this.summary = summary;
+        this.description = description;
+        this.organsAffected = organsAffected;
+    }
+
+    public MedicalProcedure(LocalDate procedureDate, String summary) {
+        this.procedureDate = procedureDate;
+        this.summary = summary;
+    }
+
+    public MedicalProcedure() {
+    }
+
+    public LocalDate getProcedureDate() {
+        return procedureDate;
+    }
+
+    public void setProcedureDate(LocalDate procedureDate) {
+        this.procedureDate = procedureDate;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ArrayList<Organs> getOrgansAffected() {
+        return organsAffected;
+    }
+
+    public void setOrgansAffected(ArrayList<Organs> organsAffected) {
+        this.organsAffected = organsAffected;
+    }
+}
