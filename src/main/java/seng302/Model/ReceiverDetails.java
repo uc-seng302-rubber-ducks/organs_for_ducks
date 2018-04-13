b833c99b7bde74e669bcc8ccf53c6efec4619cab
Tyler Kennedy <tke34@uclive.ac.nz>
2018-04-13 12:32:41 +1200

    changed organs to be a hashmap of organ to a list of times (similar to medication times) #story[602]

--- src/main/java/seng302/Model/ReceiverDetails.java
+++ src/main/java/seng302/Model/ReceiverDetails.java
@@ -2,27 +2,80 @@ package seng302.Model;
 
 import com.google.gson.annotations.Expose;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class ReceiverDetails {
-  @Expose
-  private HashSet<Organs> organs;
-  private transient User attachedUser;
 
-  public ReceiverDetails (User attachedUser){
+  @Expose
+  private transient User attachedUser;
+  private HashMap<Organs, ArrayList<LocalDate>> organs = new HashMap<>();
+
+  public ReceiverDetails(User attachedUser) {
     this.attachedUser = attachedUser;
   }
 
-  public HashSet<Organs> getOrgans() {
-    return organs;
-  }
-
-  public void setOrgans(HashSet<Organs> organs) {
-    attachedUser.updateLastModified();
-    this.organs = organs;
-  }
   //TODO model from DonorDetails (get/set/add/remove/isEmpty etc)
 
+  /**
+   * determines whether a user is waiting for the given organ
+   *
+   * @param organ organ in question
+   * @return true if organ is being waited for
+   */
+  public boolean isCurrentlyWaitingFor(Organs organ) {
+    //organ is in list and uneven number of time entries.
+    //time entries can be grouped in pairs of start/stop times. uneven would mean they are currently waiting
+    return organs.containsKey(organ) && organs.get(organ).size() % 2 == 1;
+  }
+
+  /**
+   * appends one organ to the list of organs this user is waiting for. If the user is already
+   * waiting for this organ, no change will be made.
+   */
+  public void startWaitingForOrgan(Organs organ) {
+    if (isCurrentlyWaitingFor(organ)) {
+      return;
+    }
+    //existing entry
+    if (organs.containsKey(organ)) {
+      organs.get(organ).add(LocalDate.now());
+    } else {
+      //create new entry
+      ArrayList<LocalDate> list = new ArrayList<>();
+      list.add(LocalDate.now());
+      organs.put(organ, list);
+    }
+  }
+
+  /**
+   * if the user is currently waiting for an organ, adds a timestamp to the list
+   *
+   * @param organ organ to stop waiting for
+   */
+  public void stopWaitingForOrgan(Organs organ) {
+    if (isCurrentlyWaitingFor(organ)) {
+      organs.get(organ).add(LocalDate.now());
+    }
+  }
+
+  public User getAttachedUser() {
+    return attachedUser;
+  }
+
+  /**
+   * USE SPARINGLY. this can easily create consistency issues. Only sensible use case is
+   * user.getReceiverDetails().setAttachedUser(user)
+   *
+   * @param attachedUser user to connect
+   */
+  public void setAttachedUser(User attachedUser) {
+    this.attachedUser = attachedUser;
+  }
+
+  
   /**
    * check if underlying organs list is empty TODO extend this to new functionality when added
    *
@@ -34,4 +87,5 @@ public class ReceiverDetails {
     }
     return organs.isEmpty();
   }
+
 }
