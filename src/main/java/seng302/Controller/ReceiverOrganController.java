69c1c490ac9c29fec0d38427ffe264d8b38ea99d
Aaron Bong <acb116@uclive.ac.nz>
2018-04-22 22:43:50 +1200

    Implemented functionality for organs combo box, Register button, ReRegister button and DeRegister button.
    -organs combo box now display all organs.
    -implemented functionality for Register button. when clicked, selected organ will appear at Currently Receiving List View.
    -implemented functionality for ReRegister button. when clicked, selected organ from Not Receiving List View will move to Currently Receiving List View.
    -implemented functionality for DeRegister button. when clicked, selected organ from Currently Receiving list view will move to Not Receiving List View.
    
    TODO:
    -link add organ functionality to receiver profile
    -link remove organ functionality to receiver profile
    
    #story[602]

--- src/main/java/seng302/Controller/ReceiverOrganController.java
+++ src/main/java/seng302/Controller/ReceiverOrganController.java
@@ -1,13 +1,32 @@
 package seng302.Controller;
 
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
+import seng302.Model.Organs;
 import seng302.Model.User;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ReceiverOrganController {
+
+    @FXML
+    private ComboBox<Organs> organsComboBox;
+
+    @FXML
+    private ListView<Organs> currentlyReceivingListView;
+
+    @FXML
+    private ListView<Organs> notReceivingListView;
 
     AppController controller;
     Stage stage;
     private User currentUser;
+
+    private ArrayList<Organs> organs;
 
 
     /**
@@ -22,6 +41,49 @@ public class ReceiverOrganController {
         //showCurrentDate();
         //stage.setMinWidth(620);
         //stage.setMaxWidth(620);
+        organs = new ArrayList<>(Arrays.asList(Organs.values()));
+        organsComboBox.setItems(FXCollections.observableList(organs));
+    }
+
+    /**
+     * register an organ
+     * for receiver
+     */
+    public void registerOrgan(){
+        //TODO: link add organ functionality to receiver profile
+
+        Organs toRegister = organsComboBox.getSelectionModel().getSelectedItem();
+        if(!currentlyReceivingListView.getItems().contains(toRegister)) {
+            currentlyReceivingListView.getItems().add(toRegister);
+        }
+    }
+
+    /**
+     * re-register an organ
+     * for receiver
+     */
+    public void reRegisterOrgan(){
+        //TODO: link add organ functionality to receiver profile
+
+        Organs toReRegister = notReceivingListView.getSelectionModel().getSelectedItem();
+        if(toReRegister != null) {
+            currentlyReceivingListView.getItems().add(toReRegister);
+            notReceivingListView.getItems().remove(toReRegister);
+        }
+    }
+
+    /**
+     * de-register an organ
+     * for receiver
+     */
+    public void deRegisterOrgan(){
+        //TODO: link remove organ functionality to receiver profile
+
+        Organs toDeRegister = currentlyReceivingListView.getSelectionModel().getSelectedItem();
+        if(toDeRegister != null) {
+            notReceivingListView.getItems().add(toDeRegister);
+            currentlyReceivingListView.getItems().remove(toDeRegister);
+        }
     }
 
 }
