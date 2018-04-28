5d0822607f1e9b42690d2c16aa6e41c851121527
Jennifer Halvorsen <jha236@uclive.ac.nz>
2018-04-29 01:45:29 +1200

    Detecting changes on the clinician update screen
    - added a listener for all the text fields to detect when the text field content has changed
    - an asterix is added to the end of the title bar when any text field has changed
    - the asterix is removed when all fields are back to their original state (note the original state is only reached with ctrl-z, not with backspace/retyping)
    - the ctrl-z shortcut only works on the focused text field
    - first name and region of the clinician are checked to be null when pre-filling textfields to compensate for the initial state of the default clinician
    - changed the constructor called for creating the default clinician so that null values were set appropriately
    - added a warning alert for when the clinician tries to cancel the update when there are unsaved changes
    - fixed a test in which the alert wouldn't disappear and the robot could no longer click on anything (made a custom fx:id for the alert OK button)
    - swapped around the order of a setScene and init call to avoid a null pointer exception when getting the scene
    #story[615]

--- src/main/java/seng302/Controller/UpdateClinicianController.java
+++ src/main/java/seng302/Controller/UpdateClinicianController.java
@@ -4,16 +4,18 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import seng302.Model.Clinician;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 /**
  * Controller for updating clinicians
@@ -93,12 +95,18 @@ public class UpdateClinicianController {
             staffIDTextField.setText(clinician.getStaffId());
             passwordField.setText(clinician.getPassword());
             confirmPasswordField.setText(clinician.getPassword());
-            firstNameTextField.setText(clinician.getFirstName());
-            regionTextField.setText(clinician.getRegion());
 
+            String fName = clinician.getFirstName();
             String mName = clinician.getMiddleName();
             String lName = clinician.getLastName();
             String address = clinician.getWorkAddress();
+            String region = clinician.getRegion();
+
+            if (fName == null) {
+                firstNameTextField.setText("");
+            } else {
+                firstNameTextField.setText(fName);
+            }
 
             if (mName == null) {
                 middleNameTextField.setText("");
@@ -118,11 +126,105 @@ public class UpdateClinicianController {
                 addressTextField.setText(address);
             }
 
+            if (region == null) {
+                regionTextField.setText("");
+            } else {
+                regionTextField.setText(region);
+            }
+
+            // checks if there was a change in any of the user input fields
+            changesListener(staffIDTextField);
+            changesListener(passwordField);
+            changesListener(firstNameTextField);
+            changesListener(middleNameTextField);
+            changesListener(lastNameTextField);
+            changesListener(addressTextField);
+            changesListener(regionTextField);
+
+            Scene scene = stage.getScene();
+
+            final KeyCombination shortcutZ = new KeyCodeCombination(
+                    KeyCode.Z, KeyCombination.SHORTCUT_DOWN);
+
+            scene.addEventFilter(KeyEvent.KEY_RELEASED, e -> {
+                if (shortcutZ.match(e)) {
+                    if (checkChanges()) { // checks if reverting a textfield change restores all fields to their original state
+                        stage.setTitle("Update Clinician: " + currentClinician.getFirstName());
+                    }
+                }
+            });
+
         } else if (newClinician) {
             stage.setTitle("Create New Clinician Profile");
             titleLabel.setText("Create Clinician");
             confirmButton.setText("Create Clinician Profile");
         }
+    }
+
+
+    /**
+     * Checks if all text fields are equal to their original pre-filled inputs.
+     * The pre-filled inputs are the same as the clinicians attributes.
+     * @return true if they are all equal, false if at least one is different.
+     */
+    private boolean checkChanges() {
+        boolean noChange = true;
+
+        if (!(currentClinician.getStaffId()).equals(staffIDTextField.getText())) {
+            noChange = false;
+        }
+
+        if (!(currentClinician.getPassword()).equals(passwordField.getText())) {
+            noChange = false;
+        }
+
+        if (!(currentClinician.getFirstName()).equals(firstNameTextField.getText())) {
+            noChange = false;
+        }
+
+        if (currentClinician.getMiddleName() != null) {
+            if (!(currentClinician.getMiddleName()).equals(middleNameTextField.getText())) {
+                noChange = false;
+            }
+        } else if (!middleNameTextField.getText().isEmpty()) {
+            noChange = false;
+        }
+
+        if (currentClinician.getLastName() != null) {
+            if (!(currentClinician.getLastName()).equals(lastNameTextField.getText())) {
+                noChange = false;
+            }
+        } else if (!lastNameTextField.getText().isEmpty()) {
+            noChange = false;
+        }
+
+        if (currentClinician.getWorkAddress() != null) {
+            if (!(currentClinician.getWorkAddress()).equals(addressTextField.getText())) {
+                noChange = false;
+            }
+        } else if (!addressTextField.getText().isEmpty()) {
+            noChange = false;
+        }
+
+        if (currentClinician.getRegion() != null) {
+            if (!(currentClinician.getRegion()).equals(regionTextField.getText())) {
+                noChange = false;
+            }
+        } else if (!regionTextField.getText().isEmpty()) {
+            noChange = false;
+        }
+
+        return noChange;
+    }
+
+    /**
+     * Changes the title bar to contain an asterix if a change was detected.
+     * @param field The current textfield/password field element.
+     */
+    private void changesListener(TextField field) {
+        field.textProperty().addListener((observable, oldValue, newValue) -> {
+            stage.setTitle("Update Clinician: " + currentClinician.getFirstName() + " *");
+        });
     }
 
     /**
@@ -144,6 +246,7 @@ public class UpdateClinicianController {
     }
 
     /**
+     * If changes are present, a pop up alert is displayed.
      * Closes the window without making any changes.
      * @param event an action event.
      */
@@ -151,7 +254,22 @@ public class UpdateClinicianController {
     private void cancelUpdate(ActionEvent event) {
 
         if (!newClinician) {
-            stage.close();
+            if (stage.getTitle().equals("Update Clinician: " + currentClinician.getFirstName() + " *")) { // has changes
+                Alert alert = new Alert(Alert.AlertType.WARNING,
+                        "You have unsaved changes, are you sure you want to cancel?",
+                        ButtonType.YES, ButtonType.NO);
+
+                Button yesButton = (Button) alert.getDialogPane().lookupButton(ButtonType.YES);
+                yesButton.setId("yesButton");
+
+                Optional<ButtonType> result = alert.showAndWait();
+                if (result.get() == ButtonType.YES) {
+                    stage.close();
+                }
+            } else { // has no changes
+                stage.close();
+            }
+
         } else {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/loginView.fxml"));
             Parent root = null;
@@ -178,7 +296,6 @@ public class UpdateClinicianController {
         hideErrorMessages(); // clears the error messages
         boolean valid = true;
         String staffID = staffIDTextField.getText();
-        System.out.println("here");
 
         if (!staffID.isEmpty()) {
             Clinician foundClinician = controller.getClinician(staffID);
@@ -239,7 +356,6 @@ public class UpdateClinicianController {
         }
 
         if (valid && !newClinician) { // updates an existing clinician
-            System.out.println("valid");
             // updates the attributes
             currentClinician.setStaffId(staffID);
             currentClinician.setFirstName(fName);
