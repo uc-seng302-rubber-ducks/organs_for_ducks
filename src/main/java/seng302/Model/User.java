cf5170e4c0b6a0d84a1630d511733e190e8e8cc6
Jennifer Halvorsen <jha236@uclive.ac.nz>
2018-05-01 15:17:36 +1200

    Removed an unnecessary class with safe delete and removed all occurrences  of it
    #story[602]

--- src/main/java/seng302/Model/User.java
+++ src/main/java/seng302/Model/User.java
@@ -86,13 +86,11 @@ public class User {
   @Expose
   private ReceiverDetails receiverDetails = new ReceiverDetails(this);
 
-  private Collection<Organs> commonOrgans = new HashSet<>();
+  private Collection<Organs> commonOrgans;
 
   private ArrayList<Disease> pastDiseases;
 
   private ArrayList<Disease> currentDiseases;
-
-  private OrganIntersection organIntersection;
 
 //  public User(java.time.LocalDate dateOfBirth, java.time.LocalDate dateOfDeath, String gender, double height, double weight,
 //              String bloodType,
@@ -141,7 +139,7 @@ public class User {
       this.previousMedicationTimes = new HashMap<>();
         this.donorDetails = new DonorDetails(this);
         this.receiverDetails = new ReceiverDetails(this);
-        this.organIntersection = new OrganIntersection(this);
+        this.commonOrgans = new HashSet<>();
 
       this.currentDiseases = new ArrayList<>();
       this.pastDiseases = new ArrayList<>();
@@ -203,7 +201,7 @@ public class User {
     this.nhi = nhi;
     this.donorDetails = new DonorDetails(this);
     this.receiverDetails = new ReceiverDetails(this);
-    this.organIntersection = new OrganIntersection(this);
+    this.commonOrgans = new HashSet<>();
     this.medicalProcedures = medicalProcedures;
     //TODO fix json reader
     try {
@@ -271,7 +269,7 @@ public class User {
 
     this.donorDetails = new DonorDetails(this);
     this.receiverDetails = new ReceiverDetails(this);
-    this.organIntersection = new OrganIntersection(this);
+    this.commonOrgans = new HashSet<>();
     this.medicalProcedures =  new ArrayList<>();
     try {
       changes = JsonHandler.importHistoryFromFile(name);
@@ -298,7 +296,7 @@ public class User {
     this.medicalProcedures = new ArrayList<>();
     this.donorDetails = new DonorDetails(this);
     this.receiverDetails = new ReceiverDetails(this);
-    this.organIntersection = new OrganIntersection(this);
+    this.commonOrgans = new HashSet<>();
     changes = new ArrayList<>();
   }
 
