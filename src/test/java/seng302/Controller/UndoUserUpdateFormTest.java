89aff76efe7594bf52dc3e55251c7243fc2c707e
Tyler Kennedy <tke34@uclive.ac.nz>
2018-05-02 13:53:24 +1200

    wrote stub tests for undo functions in update form #story[666]

---
+++ src/test/java/seng302/Controller/UndoUserUpdateFormTest.java
@@ -0,0 +1,52 @@
+package seng302.Controller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UndoUserUpdateFormTest {
+
+  @Before
+  public void setUp() {
+    //set up a dummy user
+  }
+
+  @Test
+  public void SingleChangeSingleUndo() {
+
+  }
+
+  @Test
+  public void NoChangeUndoDisabled() {
+
+  }
+
+  @Test
+  public void ChangesResetWhenUndoClicked() {
+
+  }
+
+  @Test
+  public void ChangesResetWhenWindowClosed() {
+    //by clicking the X
+  }
+
+  @Test
+  public void MultipleChangesSummedInMainWindow() {
+
+  }
+
+  @Test
+  public void MultipleChangesSingleUndo() {
+
+  }
+
+  @Test
+  public void MultipleChangesEqualUndos() {
+    //make sure to use different parts of the form
+  }
+
+  @Test
+  public void MultipleActionsTwoUndosOneAction() {
+    //check we can traverse the stack properly
+  }
+}
