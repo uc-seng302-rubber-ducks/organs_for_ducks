0dc8dcc43cd941a3d80f3f660f73eed146b7a516
Eiran Ling <eiranling@gmail.com>
2018-05-03 12:27:44 +1200

    Fixed test windows being hidden behind other test windows because they weren't close at the end of tests.

--- src/test/java/seng302/steps/GivenSteps.java
+++ src/test/java/seng302/steps/GivenSteps.java
@@ -1,7 +1,13 @@
 package seng302.steps;
 
+import static org.testfx.api.FxAssert.verifyThat;
+import static seng302.Utils.TableViewsMethod.getCell;
+
 import cucumber.api.PendingException;
+import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
+import java.util.ArrayList;
+import java.util.concurrent.TimeoutException;
 import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit.ApplicationTest;
 import org.testfx.matcher.control.LabeledMatchers;
@@ -9,13 +15,15 @@ import seng302.App;
 import seng302.Controller.AppController;
 import seng302.Model.User;
 
-import java.util.ArrayList;
-
-import static org.testfx.api.FxAssert.verifyThat;
-import static seng302.Utils.TableViewsMethod.getCell;
-
 public class GivenSteps extends ApplicationTest{
     private AppController controller;
+
+  @After
+  public void tearDown() throws TimeoutException {
+    if (FxToolkit.isFXApplicationThreadRunning()) {
+      FxToolkit.cleanupStages();
+    }
+  }
 
     @Given("^I have started the CLI$")
     public void iHaveStartedTheCLI() throws Throwable {
