60d4555976d0c55468f2ec297b3f7460b0b6a159
Eiran Ling <eiranling@gmail.com>
2018-05-14 10:17:10 +1200

    Merge branch 'transplantWaitingList' into undoReceivers
    # Conflicts:
    #	src/test/java/seng302/Controller/DeregisterOrganReasonControllerGUITest.java
    #	src/test/java/seng302/Controller/DeregisterOrganReasonControllerTest.java
    #	src/test/java/seng302/GUITests/DeregisterOrganReasonControllerGUITest.java

--- src/test/java/seng302/Utils/TableViewsMethod.java
+++ src/test/java/seng302/Utils/TableViewsMethod.java
@@ -1,11 +1,11 @@
 package seng302.Utils;
 
+import static org.loadui.testfx.GuiTest.find;
+
 import javafx.scene.Node;
 import javafx.scene.control.TableView;
 import org.loadui.testfx.controls.TableViews;
 import org.loadui.testfx.exceptions.NoNodesFoundException;
-
-import static org.loadui.testfx.GuiTest.find;
 
 /**
  * contains methods that manipulates javaFX TableView elements
@@ -52,4 +52,18 @@ public class TableViewsMethod extends TableViews{
      }
     return (TableView<?>) node;
     }
+
+  /**
+   * gets number of rows of a table view
+   *
+   * @param tableName table name
+   * @return number of rows, 0 if none.
+   */
+  public static int getNumberOfRows(String tableName) {
+    try {
+      return numberOfRowsIn(tableName);
+    } catch (NullPointerException e) { //NPE only gets triggered when LoadUI tries to count number of rows of empty table.
+      return 0;
+    }
+  }
 }
