e5b85b74615f00b03eb8a5277d9c653579abd0cd
Josh Burt <jbu71@uclive.ac.nz>
2018-05-30 00:34:13 +1200

    CSV Import has arrived
    -Added a CSVparser
    -Added a first run at decoding the CSV, Still needs to be run and tested before being updated to match User re-engineer. All fields are removed from the CSV record however.
    
    Still to do:
    Run it.
    Test it.
    Call it
    
    #story[810]

---
+++ src/main/java/seng302/utils/CSVHandler.java
@@ -0,0 +1,106 @@
+package seng302.utils;
+
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import seng302.model.User;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * Class to handle the importing and redirection of the CSV data.
+ *
+ * TODO: Extend FileHandler when file handler is written
+ */
+public class CSVHandler {
+
+
+    /**
+     *Takes a CSV file, opened by the file handler
+     * and reads the file into a list separated by record
+     *
+     * Throws an IOException if an incorrect file is passed into it.
+     *
+     * @param csvFile CSV File to be parsed
+     * @return A list of CSV records in the file
+     */
+    public List<CSVRecord> parseCSV(File csvFile) throws IOException {
+        Reader reader = new FileReader(csvFile);
+        CSVParser parser = CSVParser.parse(reader, CSVFormat.RFC4180);
+        return parser.getRecords();
+    }
+
+
+    /**
+     * Decodes the results of a parsed CSV file into a Collection of user classes
+     *
+     *
+     * @param records List of records to be decoded
+     * @return Collection of Users from file
+     */
+    public Collection<User> decodeUsersFromCSV(List<CSVRecord> records){
+        Collection<User> users = new HashSet<>();
+        int count = 0;
+        int malformed = 0;
+        int correct = 0;
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/YYYY");
+
+        for (CSVRecord record : records){
+            count++;
+            // as column name maybe null this needs to be done by index
+            User u = new User();
+            u.setNhi(record.get(0));
+            u.setFirstName(record.get(1));
+            u.setLastName(record.get(2));
+            try {
+                u.setDateOfBirth(LocalDate.parse(record.get(3), dtf));
+                if (!record.get(4).equals("")){
+                    //TODO: Make this a LocalDateTime when Time of death is added
+                    //here it is assumed that a non-empty DOD column implies a valid DOD should exist
+                    u.setDateOfDeath(LocalDate.parse(record.get(4), dtf));
+                }
+                u.setHeight(Double.parseDouble(record.get(8)) / 100);
+                u.setWeight(Double.parseDouble(record.get(9)));
+            } catch (DateTimeParseException | NumberFormatException ex){
+                // catches a format error and then breaks that one malformed statement
+                Log.warning("An invalid  value was detected in record: "+ count, ex);
+                malformed++;
+                continue;
+            }
+            u.setBirthGender(record.get(5));
+            u.setGenderIdentity(record.get(6));
+            u.setBloodType(record.get(7));
+            String streetNumber = record.get(10);
+            String streetName = record.get(11);
+            String neighbourhood = record.get(12);
+            String city = record.get(13);
+            String zipCode = record.get(15);
+            //TODO: change this to new address format
+            u.setCurrentAddress(streetNumber+ " " + streetName + " " + neighbourhood + " " + city + " " + zipCode);
+            u.setRegion(record.get(14));
+            //String country = record.get(16);
+            //String birthCountry = record.get(17);
+            u.setHomePhone(record.get(18));
+            u.setCellPhone(record.get(19));
+            u.setEmail(record.get(20));
+            correct++;
+            users.add(u);
+        }
+        Log.info("Finished Decoding File\n" +
+                "Records in File: " + count +
+                "\nRecords Successfully Read: " + correct +
+                "\nNumber of Malformed Records " + malformed);
+        return users;
+    }
+
+}
