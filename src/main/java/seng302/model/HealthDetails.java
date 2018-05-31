e5075e74e18dccfd0bab7a621c35f67dae81ad56
Jennifer Halvorsen <jha236@uclive.ac.nz>
2018-05-31 13:36:04 +1200

    Branch to update the user to match sample data
    - created a class to separate out the health details
    #story[810]

---
+++ src/main/java/seng302/model/HealthDetails.java
@@ -0,0 +1,14 @@
+package seng302.model;
+
+/**
+ * Class for health details for a user
+ */
+public class HealthDetails {
+
+    private String birthGender;
+    private String genderIdentity;
+    private String alcoholConsumption;
+    private boolean smoker;
+
+
+}
