d1ad4b7d5784046281fe3caaff07d0254fa1ad07
Eiran Ling <eiranling@gmail.com>
2018-07-11 16:17:30 +1200

    Renamed packages to match sonarlint convention

--- server/src/test/java/odms/controller/userDetailsControllerTests/MedicationControllerTest.java
+++ server/src/test/java/odms/controller/userDetailsControllerTests/MedicationControllerTest.java
@@ -4,7 +4,7 @@ import odms.commons.model.User;
 import odms.commons.model.datamodel.Medication;
 import odms.commons.utils.DBHandler;
 import odms.commons.utils.JDBCDriver;
-import odms.controller.userDetailsControllers.MedicationController;
+import odms.controller.user.details.controllers.MedicationController;
 import odms.exception.ServerDBException;
 import odms.utils.DBManager;
 import org.junit.Assert;
