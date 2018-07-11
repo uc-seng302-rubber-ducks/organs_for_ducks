65310d1e2b4c9f46cd6f5a06a1baa622c8c48d92
Eiran Ling <eiranling@gmail.com>
2018-07-11 16:15:45 +1200

    Added medication endpoints and status code tests. #story[821] TODO: - Add tests to check whether the medications are placed in the right lists.

---
+++ server/src/main/java/odms/controller/userDetailsControllers/MedicationController.java
@@ -0,0 +1,95 @@
+package odms.controller.userDetailsControllers;
+
+import odms.commons.model.User;
+import odms.commons.model.datamodel.Medication;
+import odms.commons.utils.DBHandler;
+import odms.commons.utils.JDBCDriver;
+import odms.commons.utils.Log;
+import odms.controller.BaseController;
+import odms.controller.OdmsController;
+import odms.exception.ServerDBException;
+import odms.security.IsClinician;
+import odms.utils.DBManager;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+@OdmsController
+public class MedicationController extends BaseController {
+
+    private DBHandler handler;
+    private JDBCDriver driver;
+
+    public MedicationController(DBManager manager) {
+        super(manager);
+        handler = super.getHandler();
+        driver = super.getDriver();
+    }
+
+    @IsClinician
+    @RequestMapping(method = RequestMethod.POST, value = "/users/{nhi}/medications")
+    public ResponseEntity postMedication(@PathVariable("nhi") String nhi,
+                                         @RequestBody Medication medication) {
+        try (Connection connection = driver.getConnection()) {
+            User user = handler.getOneUser(connection, nhi);
+            if (user == null) {
+                return new ResponseEntity(HttpStatus.NOT_FOUND);
+            }
+            addMedicationToCorrectList(medication, user.getPreviousMedication(), user.getCurrentMedication());
+            handler.saveUser(user, connection);
+        } catch (SQLException ex) {
+            Log.severe("Cannot add medications to user " + nhi, ex);
+            throw new ServerDBException(ex);
+        }
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @IsClinician
+    @RequestMapping(method = RequestMethod.PUT, value = "/users/{nhi}/medications")
+    public ResponseEntity putMedications(@PathVariable("nhi") String nhi,
+                                         @RequestBody List<Medication> medications) {
+        try (Connection connection = driver.getConnection()) {
+            User user = handler.getOneUser(connection, nhi);
+            if (user == null) {
+                return new ResponseEntity(HttpStatus.NOT_FOUND);
+            }
+            Collection<Medication> currentMedications = new HashSet<>();
+            Collection<Medication> previousMedications = new HashSet<>();
+            for (Medication medication : medications) {
+                addMedicationToCorrectList(medication, previousMedications, currentMedications);
+            }
+            user.setCurrentMedication(new ArrayList<>(currentMedications));
+            user.setPreviousMedication(new ArrayList<>(previousMedications));
+            handler.saveUser(user, connection);
+        } catch (SQLException ex) {
+            Log.severe("Failed to update the medications for user " + nhi, ex);
+            throw new ServerDBException(ex);
+        }
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    /**
+     * Determines if a medication belongs to current or past medications and adds it to the appropriate list
+     *
+     * @param medication medication to determine
+     * @param past       Collection to add to if the medication was taken in the past
+     * @param current    Collection to add to if the medication is being taken
+     */
+    private void addMedicationToCorrectList(Medication medication, Collection<Medication> past, Collection<Medication> current) {
+        if (medication.getMedicationTimes().size() % 2 == 0) {
+            past.add(medication);
+        } else {
+            current.add(medication);
+        }
+    }
+}
---
+++ server/src/test/java/odms/controller/userDetailsControllerTests/MedicationControllerTest.java
@@ -0,0 +1,93 @@
+package odms.controller.userDetailsControllerTests;
+
+import odms.commons.model.User;
+import odms.commons.model.datamodel.Medication;
+import odms.commons.utils.DBHandler;
+import odms.commons.utils.JDBCDriver;
+import odms.controller.userDetailsControllers.MedicationController;
+import odms.exception.ServerDBException;
+import odms.utils.DBManager;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class MedicationControllerTest {
+
+    private MedicationController controller;
+    private Connection connection;
+    private JDBCDriver driver;
+    private DBManager manager;
+    private DBHandler handler;
+    private User testUser;
+
+    @Before
+    public void setUp() throws SQLException {
+        connection = mock(Connection.class);
+        manager = mock(DBManager.class);
+        handler = mock(DBHandler.class);
+        driver = mock(JDBCDriver.class);
+        when(driver.getConnection()).thenReturn(connection);
+        when(manager.getHandler()).thenReturn(handler);
+        when(manager.getDriver()).thenReturn(driver);
+        controller = new MedicationController(manager);
+        testUser = new User("steve", LocalDate.now(), "ABC1234");
+    }
+
+    @Test
+    public void postProcedureShouldReturnCreated() throws SQLException {
+        when(handler.getOneUser(any(Connection.class), anyString())).thenReturn(testUser);
+        ResponseEntity res = controller.postMedication("ABC1234", new Medication("panadol"));
+        Assert.assertEquals(HttpStatus.CREATED, res.getStatusCode());
+    }
+
+    @Test
+    public void postProcedureWithNoUserShouldReturnNotFound() {
+        ResponseEntity res = controller.postMedication("ANC1111", new Medication("panadol"));
+        Assert.assertEquals(HttpStatus.NOT_FOUND, res.getStatusCode());
+    }
+
+    @Test(expected = ServerDBException.class)
+    public void postProcedureShouldThrowExceptionWhenNoConnection() throws SQLException {
+        when(driver.getConnection()).thenThrow(new SQLException());
+        controller.postMedication("ABC1234", new Medication("panadol"));
+    }
+
+    @Test
+    public void putProceduresShouldReturnOK() throws SQLException {
+        when(handler.getOneUser(any(Connection.class), anyString())).thenReturn(testUser);
+        List<Medication> medications = new ArrayList<>(Arrays.asList(new Medication("aspirin"),
+                new Medication("panadol")));
+        ResponseEntity res = controller.putMedications("ABC1234", medications);
+        Assert.assertEquals(HttpStatus.OK, res.getStatusCode());
+    }
+
+    @Test
+    public void putProceduresShouldReturnNotFoundWhenNoUser() {
+        List<Medication> medications = new ArrayList<>(Arrays.asList(new Medication("aspirin"),
+                new Medication("panadol")));
+        ResponseEntity res = controller.putMedications("ABC1111", medications);
+        Assert.assertEquals(HttpStatus.NOT_FOUND, res.getStatusCode());
+    }
+
+    @Test(expected = ServerDBException.class)
+    public void putProceduresShouldThrowExceptionWhenNoConnection() throws SQLException {
+        when(driver.getConnection()).thenThrow(new SQLException());
+        List<Medication> medications = new ArrayList<>(Arrays.asList(new Medication("aspirin"),
+                new Medication("panadol")));
+        controller.putMedications("ABC1234", medications);
+    }
+}
