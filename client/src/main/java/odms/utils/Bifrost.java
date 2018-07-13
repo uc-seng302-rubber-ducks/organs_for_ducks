74abe1676912a067fec328af6e51311fba4c1467
Eiran Ling <eiranling@gmail.com>
2018-07-13 16:15:35 +1200

    Added the bifrost class, and started to make some headway on it. #story[821]

---
+++ client/src/main/java/odms/utils/Bifrost.java
@@ -0,0 +1,63 @@
+package odms.utils;
+
+import com.google.gson.Gson;
+import odms.commons.model.User;
+import odms.commons.model.dto.UserOverview;
+import odms.commons.utils.Log;
+import odms.controller.AppController;
+import okhttp3.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Serves as a link between the client and the server
+ */
+public class Bifrost {
+
+    private OkHttpClient client;
+    private static Bifrost instance;
+    private String ip;
+
+    private Bifrost(OkHttpClient client, String ip) {
+        this.client = client;
+        this.ip = ip;
+    }
+
+    public static Bifrost getInstance() {
+        if (instance == null) {
+            instance = new Bifrost(new OkHttpClient(), "http://localhost:4941");
+        }
+        return instance;
+    }
+
+    public List<User> getUsers(AppController controller) {
+        String url = ip + "/api/v1/users?startIndex=0&count=4";
+        Request request = new Request.Builder().url(url).build();
+        User toReturn = null;
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.warning("Could not execute call to /users/{nhi}");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    ArrayList<UserOverview> users = new Gson().fromJson(response.body().string(), ArrayList.class);
+                    System.out.println(users);
+                    for (UserOverview user : users) {
+                        System.out.println(user);
+                    }
+                }
+            }
+        });
+        return controller.getUsers();
+    }
+
+    public static void main(String[] argv) {
+        Bifrost bifrost = Bifrost.getInstance();
+        System.out.println(bifrost.getUsers(AppController.getInstance()));
+    }
+}
