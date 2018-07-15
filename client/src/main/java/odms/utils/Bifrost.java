cc3a65b9aed3b1daf9d3b6bb49358bbfe931f5db
Eiran Ling <eiranling@gmail.com>
2018-07-16 11:40:54 +1200

    Added more methods to the user bridge #story[821]

--- client/src/main/java/odms/utils/Bifrost.java
+++ client/src/main/java/odms/utils/Bifrost.java
@@ -3,25 +3,21 @@ package odms.utils;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import odms.commons.model.Clinician;
-import odms.commons.model.User;
-import odms.commons.model.dto.UserOverview;
 import odms.commons.utils.Log;
 import odms.controller.AppController;
 import okhttp3.*;
 
 import java.io.IOException;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
  * Serves as a link between the client and the server
  */
 public class Bifrost {
 
-    private OkHttpClient client;
-    private String ip;
-    private MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    protected OkHttpClient client;
+    protected String ip;
+    protected MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
     public Bifrost(OkHttpClient client, String ip) {
         this.client = client;
@@ -30,32 +26,6 @@ public class Bifrost {
 
     public Bifrost(OkHttpClient client) {
         this(client, "http://localhost:4941/odms/v1");
-    }
-
-    public void getUsers(AppController controller, int startIndex, int count, String name, String region) {
-        String url = ip + "/users?startIndex=" + startIndex + "&count=" + count;
-        Request request = new Request.Builder().url(url).build();
-        User toReturn = null;
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                Log.warning("Could not execute call to /users/{nhi}");
-                e.printStackTrace();
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                if (response.isSuccessful()) {
-                    try (ResponseBody body = response.body()) {
-                        Set<UserOverview> users = new Gson().fromJson(body.string(), new TypeToken<HashSet<UserOverview>>() {
-                        }.getType());
-                        controller.setUserOverviews(users);
-                    }
-                } else {
-                    throw new IOException("Unexpected code " + response);
-                }
-            }
-        });
     }
 
     public void getClinicians(AppController controller, int startIndex, int count, String token) {
@@ -79,42 +49,5 @@ public class Bifrost {
                 }
             }
         });
-    }
-
-    public void postUser(User user) {
-        String url = ip + "/users";
-        RequestBody requestBody = RequestBody.create(JSON, new Gson().toJson(user));
-        Request request = new Request.Builder().post(requestBody).url(url).build();
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                Log.warning("Could not make the call to POST /users");
-                e.printStackTrace();
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                //Don't need to do anything with the response
-            }
-        });
-    }
-
-    public User getUser(String nhi) throws IOException {
-        User toReturn;
-        String url = ip + "/users/" + nhi;
-        Request request = new Request.Builder().url(url).build();
-        try {
-            try (Response response = client.newCall(request).execute()) {
-                if (response.isSuccessful()) {
-                    toReturn = new Gson().fromJson(response.body().string(), User.class);
-                } else {
-                    toReturn = null;
-                }
-            }
-        } catch (IOException ex) {
-            Log.warning("Could not make GET call to /users/" + nhi, ex);
-            throw ex;
-        }
-        return toReturn;
     }
 }
--- client/src/main/java/odms/utils/UserBridge.java
+++ client/src/main/java/odms/utils/UserBridge.java
@@ -1,8 +1,23 @@
 package odms.utils;
 
-import okhttp3.OkHttpClient;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import odms.commons.model.Disease;
+import odms.commons.model.MedicalProcedure;
+import odms.commons.model.User;
+import odms.commons.model.datamodel.Medication;
+import odms.commons.model.dto.UserOverview;
+import odms.commons.utils.Log;
+import odms.controller.AppController;
+import okhttp3.*;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class UserBridge extends Bifrost {
+
     public UserBridge(OkHttpClient client, String ip) {
         super(client, ip);
     }
@@ -10,4 +25,221 @@ public class UserBridge extends Bifrost {
     public UserBridge(OkHttpClient client) {
         super(client);
     }
+
+    public void loadUsersToController(AppController controller, int startIndex, int count, String name, String region) {
+        String url = ip + "/users?startIndex=" + startIndex + "&count=" + count;
+        Request request = new Request.Builder().url(url).build();
+        User toReturn = null;
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.warning("Could not execute call to /users/{nhi}");
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    try (ResponseBody body = response.body()) {
+                        Set<UserOverview> users = new Gson().fromJson(body.string(), new TypeToken<HashSet<UserOverview>>() {
+                        }.getType());
+                        controller.setUserOverviews(users);
+                    }
+                } else {
+                    throw new IOException("Unexpected code " + response);
+                }
+            }
+        });
+    }
+
+    public void postUser(User user) {
+        String url = ip + "/users";
+        RequestBody requestBody = RequestBody.create(JSON, new Gson().toJson(user));
+        Request request = new Request.Builder().post(requestBody).url(url).build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.warning("Could not make the call to POST /users");
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    throw new IOException("Failed to make POST call to /users");
+                }
+            }
+        });
+    }
+
+    public User getUser(String nhi) throws IOException {
+        User toReturn;
+        String url = ip + "/users/" + nhi;
+        Request request = new Request.Builder().url(url).build();
+        try {
+            try (Response response = client.newCall(request).execute()) {
+                if (response.isSuccessful()) {
+                    toReturn = new Gson().fromJson(response.body().string(), User.class);
+                } else {
+                    toReturn = null;
+                }
+            }
+        } catch (IOException ex) {
+            Log.warning("Could not make GET call to /users/" + nhi, ex);
+            throw ex;
+        }
+        return toReturn;
+    }
+
+    public void putUser(User user, String nhi) {
+        String url = ip + "/users/" + nhi;
+        RequestBody body = RequestBody.create(JSON, new Gson().toJson(user));
+        Request request = new Request.Builder().url(url).put(body).build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.warning("Could not PUT to " + url, e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    throw new IOException("Failed to PUT to " + url);
+                }
+            }
+        });
+    }
+
+    public void deleteUser(User user) {
+        String url = ip + "/users/" + user.getNhi();
+        Request request = new Request.Builder().url(url).delete().build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.warning("Could not DELETE " + url, e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    throw new IOException("Failed to DELETE to " + url);
+                }
+            }
+        });
+    }
+
+    public void postUserProcedures(List<MedicalProcedure> procedures, String nhi) {
+        String url = ip + "/users/" + nhi + "/procedures";
+        RequestBody body = RequestBody.create(JSON, new Gson().toJson(procedures));
+        Request request = new Request.Builder().url(url).post(body).build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.warning("Could not POST procedures to " + url, e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    throw new IOException("Failed to POST procedures to " + url);
+                }
+            }
+        });
+    }
+
+    public void putUserProcedures(List<MedicalProcedure> procedures, String nhi) {
+        String url = ip + "/users/" + nhi + "/procedures";
+        RequestBody body = RequestBody.create(JSON, new Gson().toJson(procedures));
+        Request request = new Request.Builder().url(url).put(body).build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.warning("Could not PUT procedures to " + url, e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    throw new IOException("Failed to PUT to " + url);
+                }
+            }
+        });
+    }
+
+    public void postMedications(List<Medication> medications, String nhi) {
+        String url = ip + "/users/" + nhi + "/medications";
+        RequestBody body = RequestBody.create(JSON, new Gson().toJson(medications));
+        Request request = new Request.Builder().url(url).post(body).build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.warning("Could not POST procedures to " + url, e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    throw new IOException("Failed to POST to " + url);
+                }
+            }
+        });
+    }
+
+    public void putMedications(List<Medication> medications, String nhi) {
+        String url = ip + "/users/" + nhi + "/medications";
+        RequestBody body = RequestBody.create(JSON, new Gson().toJson(medications));
+        Request request = new Request.Builder().url(url).put(body).build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.warning("Could not PUT procedures to " + url, e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    throw new IOException("Failed to PUT to " + url);
+                }
+            }
+        });
+    }
+
+    public void postDiseases(List<Disease> diseases, String nhi) {
+        String url = ip + "/users/" + nhi + "/diseases";
+        RequestBody body = RequestBody.create(JSON, new Gson().toJson(diseases));
+        Request request = new Request.Builder().url(url).post(body).build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.warning("Could not POST diseases to " + url);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    throw new IOException("Failed to POST to " + url);
+                }
+            }
+        });
+    }
+
+    public void putDiseases(List<Disease> diseases, String nhi) {
+        String url = ip + "/users/" + nhi + "/diseases";
+        RequestBody body = RequestBody.create(JSON, new Gson().toJson(diseases));
+        Request request = new Request.Builder().url(url).put(body).build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.warning("Could not PUT to " + url);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    throw new IOException("Failed to PUT to " + url);
+                }
+            }
+        });
+    }
+
 }
