5e16eb3e9dd61a6ebc5e42259a3466b88b3e73e4
jre103 <jre103@uclive.ac.nz>
2018-07-31 21:58:50 +1200

    Created GUI for editing death details
    #story[1016]

--- client/src/main/java/odms/controller/gui/popup/UpdateDeathDetailsController.java
+++ client/src/main/java/odms/controller/gui/popup/UpdateDeathDetailsController.java
@@ -1,8 +1,11 @@
 package odms.controller.gui.popup;
 
+import javafx.fxml.FXML;
 import javafx.stage.Stage;
 import odms.commons.model.User;
+import odms.commons.utils.Log;
 import odms.controller.AppController;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /**
  * Controller class for editing death details of a user
@@ -18,4 +21,32 @@ public class UpdateDeathDetailsController {
         this.stage = stage;
         this.currentUser = currentUser;
     }
+
+    @FXML
+    private void cancelUpdateDeathDetails() {
+        closeUpdateDeathDetails();
+    }
+
+    /**
+     * Closes the update death details popup. Called by the Cancel button
+     * (Empty for now)
+     */
+    private void closeUpdateDeathDetails(){
+        Log.severe("Not implemented yet", new NotImplementedException());
+    }
+
+    @FXML
+    private void confirmUpdateDeathDetails() {
+        saveUpdateDeathDetails();
+        closeUpdateDeathDetails();
+    }
+
+    /**
+     * Saves the update death details changes to the user. Called by the Confirm button
+     * (Empty for now)
+     */
+    private void saveUpdateDeathDetails() {
+        Log.severe("Not implemented yet", new NotImplementedException());
+    }
+
 }
