bf25f1d8d513f4fd76f85cac6fa1ab93ce212558
Jennifer Halvorsen <jha236@uclive.ac.nz>
2018-08-02 18:16:19 +1200

    Changed the user CLI delete to find the user by directly using the bridge
    - removed an unused scanner
    - changed the print statements to use the IoHelper display
    - updated the user delete tests
    
    story[1006]

--- client/src/test/java/odms/commands/DeleteUserTest.java
+++ client/src/test/java/odms/commands/DeleteUserTest.java
@@ -2,42 +2,50 @@ package odms.commands;
 
 import odms.controller.AppController;
 import odms.commons.model.User;
+import odms.utils.UserBridge;
 import odms.view.CLI;
 import org.junit.Before;
 import org.junit.Test;
 import picocli.CommandLine;
 
+import java.io.IOException;
 import java.time.LocalDate;
 
 import static org.mockito.Mockito.*;
 
 public class DeleteUserTest {
 
-    private AppController mockController = mock(AppController.class);
+    private AppController mockController;
     private User testUser;
+    private DeleteUser command;
+    private UserBridge userBridge;
 
     @Before
     public void CreateUser() {
+        mockController = mock(AppController.class);
         testUser = new User("testUser", LocalDate.of(1987, 4, 3), "ABC1234");
+        command = new DeleteUser();
+        userBridge = mock(UserBridge.class);
+        when(mockController.getUserBridge()).thenReturn(userBridge);
+        command.setController(mockController);
     }
 
     @Test
-    public void UserCanBeDeleted() {
+    public void UserCanBeDeleted() throws IOException {
         String[] args = {"ABC1234"};
-        DeleteUser command = new DeleteUser();
-        when(mockController.findUser("ABC1234")).thenReturn(testUser);
-        command.setController(mockController);
+
+        when(userBridge.getUser(testUser.getNhi())).thenReturn(testUser);
         new CommandLine(command).parseWithHandler(new CommandLine.RunLast(), System.err, args);
         CLI.parseInput("y", mockController);
         verify(mockController).deleteUser(testUser);
+        assert (!userBridge.getExists(testUser.getNhi()));
     }
 
     @Test
-    public void NonExistingUserCannotBeDeleted() {
+    public void NonExistingUserCannotBeDeleted() throws IOException {
         String[] args = {"ABC1234"};
-        DeleteUser command = new DeleteUser();
-        when(mockController.findUser("ABC1234")).thenReturn(null);
-        command.setController(mockController);
+
+        when(userBridge.getUser(testUser.getNhi())).thenReturn(null);
         new CommandLine(command).parseWithHandler(new CommandLine.RunLast(), System.err, args);
         CLI.parseInput("y", mockController);
         verify(mockController, times(0)).deleteUser(testUser);
