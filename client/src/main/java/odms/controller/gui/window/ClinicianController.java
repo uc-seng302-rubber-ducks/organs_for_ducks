141e191217c8868944fe40cfa697c3039fd3edac
reida66 <alanna.reid.26@gmail.com>
2018-08-04 16:58:38 +1200

    fixed profile images #story[1012]

--- client/src/main/java/odms/controller/gui/window/ClinicianController.java
+++ client/src/main/java/odms/controller/gui/window/ClinicianController.java
@@ -198,6 +198,7 @@ public class ClinicianController implements PropertyChangeListener, TransplantWa
         }
 
         displayImage(profileImage, clinician.getProfilePhotoFilePath());
+        clinician.getUndoStack().clear();
 
     }
 
