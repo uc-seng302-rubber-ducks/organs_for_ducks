1399a811f5ee416fe13054f20da33d4139095690
Eiran Ling <eiranling@gmail.com>
2018-08-08 18:32:05 +1200

    Populated the table view

--- client/src/main/java/odms/controller/gui/panel/view/AvailableOrgansViewController.java
+++ client/src/main/java/odms/controller/gui/panel/view/AvailableOrgansViewController.java
@@ -1,26 +1,47 @@
 package odms.controller.gui.panel.view;
 
+import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.ProgressBarTableCell;
+import javafx.scene.control.cell.PropertyValueFactory;
 import odms.commons.model._enum.Organs;
+import odms.commons.model.datamodel.AvailableOrganDetail;
 import odms.controller.gui.panel.logic.AvailableOrgansLogicController;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class AvailableOrgansViewController {
 
     @FXML
-    private TableView availableOrgansTableView;
+    private TableView<AvailableOrganDetail> availableOrgansTableView;
 
     @FXML
-    private ComboBox availableOrganFilterComboBox;
+    private ComboBox<String> availableOrganFilterComboBox;
 
     @FXML
-    private TextField regionFilterComboBox;
+    private TextField regionFilterTextField;
+
+    @FXML
+    private TableColumn<AvailableOrganDetail, String> nhiColumn;
+
+    @FXML
+    private TableColumn<AvailableOrganDetail, String> regionColumn;
+
+    @FXML
+    private TableColumn<AvailableOrganDetail, String> organColumn;
+
+    @FXML
+    private TableColumn<AvailableOrganDetail, LocalDateTime> deathMomentColumn;
+
+    @FXML
+    private TableColumn<AvailableOrganDetail, ProgressBarTableCell<LocalDateTime>> progressBarColumn;
 
     private AvailableOrgansLogicController logicController = new AvailableOrgansLogicController();
 
@@ -34,6 +55,11 @@ public class AvailableOrgansViewController {
     }
 
     private void initAvailableOrgansTableView() {
+        nhiColumn.setCellValueFactory(new PropertyValueFactory<>("donorNhi"));
+        regionColumn.setCellValueFactory(new PropertyValueFactory<>("region"));
+        organColumn.setCellValueFactory(new PropertyValueFactory<>("organ"));
+        deathMomentColumn.setCellValueFactory(new PropertyValueFactory<>("momentOfDeath"));
+        // figure out how to do progress bars
         // initialise table columns here
     }
 
