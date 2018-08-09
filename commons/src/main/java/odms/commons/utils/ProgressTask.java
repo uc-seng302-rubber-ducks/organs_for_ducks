1658e3f2d0c9507dbd30db8e8b3ce43324826988
reida66 <alanna.reid.26@gmail.com>
2018-08-09 14:37:08 +1200

    Tried to link the progress bar to the progress task
    #story[1017] #pair[are66, jha236]

--- commons/src/main/java/odms/commons/utils/ProgressTask.java
+++ commons/src/main/java/odms/commons/utils/ProgressTask.java
@@ -1,11 +1,23 @@
 package odms.commons.utils;
 
 import javafx.concurrent.Task;
+import javafx.scene.control.ProgressIndicator;
 
 public class ProgressTask extends Task<Void> {
+    private final Double time;
+
+    ProgressTask(Double time) {
+        this.time = time;
+    }
 
     @Override
     protected Void call() throws Exception {
+        this.updateProgress(ProgressIndicator.INDETERMINATE_PROGRESS, 1);
+        for (int i = 0; i < time; i++) {
+            updateProgress((1.0 * i) / time, 1);
+            Thread.sleep(100);
+        }
+        this.updateProgress(1, 1);
         return null;
     }
 }
