285d008b0ec4238f9bec0cf35b98f5f291e8e3d9
Tyler Kennedy <tke34@uclive.ac.nz>
2018-08-09 14:48:03 +1200

    #fix time validation that was causing a test to fail #pair[tke34, eli26]

--- commons/src/main/java/odms/commons/utils/AttributeValidation.java
+++ commons/src/main/java/odms/commons/utils/AttributeValidation.java
@@ -211,6 +211,6 @@ public class AttributeValidation {
      * @return True if string is in correct format
      */
     public static boolean validateTimeString(String time) {
-        return time.matches("([0-2][0-3]):([0-5][0-9])");
+        return time.matches("(([01][0-9])|(2[0-3])):([0-5][0-9])");
     }
 }
