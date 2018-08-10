7f70875c1683dfb9bf3076d2c14ee6bc9e4bf622
Aaron Bong <acb116@uclive.ac.nz>
2018-08-10 18:56:55 +1200

    Implemented a skeleton code for GET matching organs API Endpoint. Implemented DTO with mapping to store matching organs info
    #story[1028]

--- server/src/main/java/odms/controller/OrgansController.java
+++ server/src/main/java/odms/controller/OrgansController.java
@@ -1,12 +1,14 @@
 package odms.controller;
 
 import odms.commons.model.datamodel.AvailableOrganDetail;
+import odms.commons.model.datamodel.TransplantDetails;
 import odms.commons.utils.DBHandler;
 import odms.commons.utils.JDBCDriver;
 import odms.exception.ServerDBException;
 import odms.security.IsClinician;
 import odms.utils.DBManager;
 import org.jline.utils.Log;
+import org.springframework.data.util.Pair;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -42,6 +44,23 @@ public class OrgansController extends BaseController {
             return handler.getAvailableOrgans(startIndex, count, organ, region, bloodType, city, country, connection);
         } catch (SQLException e) {
             Log.error("Unable to retrieve organs from Db", e);
+            throw new ServerDBException(e);
+        }
+    }
+
+    @IsClinician
+    @RequestMapping(method = RequestMethod.GET, value="/matchingOrgans")
+    public List<TransplantDetails> getMatchingOrgans(@RequestParam(value = "organ") String organ,
+                                                     @RequestParam(value = "bloodType") String bloodType,
+                                                     @RequestParam(value = "city", defaultValue = "", required = false) String city,
+                                                     @RequestParam(value = "region") String region,
+                                                     @RequestParam(value = "country") String country){
+        try(Connection connection = driver.getConnection()){
+            Log.info("Getting all matching organs");
+//            return handler.; //TODO: lik to db handler once implemented
+            return null;
+        } catch (SQLException e) {
+            Log.error("Unable to retrieve matching organs from Db", e);
             throw new ServerDBException(e);
         }
     }
