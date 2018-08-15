320d5b2f73f8a7e5bded75f7c2326ffab9b68e07
Eiran Ling <eiranling@gmail.com>
2018-08-15 16:24:44 +1200

    Added javadoc for the organs bridge class

--- client/src/main/java/odms/bridge/OrgansBridge.java
+++ client/src/main/java/odms/bridge/OrgansBridge.java
@@ -19,6 +19,18 @@ public class OrgansBridge extends Bifrost {
         super(client);
     }
 
+    /**
+     * Gets all the organs available for donation by making a request to the server and populating the observable list.
+     *
+     * @param startIndex     the position to start obtaining items from
+     * @param count          how many entries to obtain
+     * @param organ          if specified, return only organs of that type
+     * @param region         if specified, return only organs located within that region
+     * @param bloodType      if specified, return only organs of that blood type
+     * @param city           if specified, return only organs in that city
+     * @param country        if specified, return only organs in that country
+     * @param observableList observable list to populate.
+     */
     public void getAvailableOrgansList(int startIndex, int count, String organ, String region, String bloodType, String city, String country, ObservableList<AvailableOrganDetail> observableList) {
         StringBuilder url = new StringBuilder(ip);
         url.append("/availableOrgans?count=").append(count);
@@ -69,6 +81,15 @@ public class OrgansBridge extends Bifrost {
 
     }
 
+    /**
+     * Gets the potential matches list from the server by firing a HTTP request.
+     * @param startIndex the position to start obtaining items from
+     * @param count how many entries to obtain
+     * @param donorNhi user who is donating the organ
+     * @param organ organ being donated
+     * @param organToDonate Available organ detail to identify the map entry of the response
+     * @param observableList the observable list to populate the potential matches with
+     */
     public void getMatchingOrgansList(int startIndex, int count, String donorNhi, String organ, AvailableOrganDetail organToDonate,
                                       ObservableList<TransplantDetails> observableList) {
         StringBuilder url = new StringBuilder(ip);
