918a8df05c239f0c20f6c0cc30b75aa105492623
Josh Burt <jbu71@uclive.ac.nz>
2018-08-16 01:47:16 +1200

    Transplant table now kind of works if it doesnt just repeat the action
    #pair[jha236, jbu71]
    #story[1006]

--- client/src/main/java/odms/bridge/TransplantBridge.java
+++ client/src/main/java/odms/bridge/TransplantBridge.java
@@ -41,7 +41,7 @@ public class TransplantBridge extends Bifrost {
         if (!organs.isEmpty()) {
             for (Organs organ : organs) {
                 //repeated values are interpreted by the server as an array
-                url.append("&organs=").append(organ.name());
+                url.append("&organs=").append(organ.name().replaceAll(" ", "_"));
             }
         }
         Log.debug(url.toString());
@@ -59,9 +59,12 @@ public class TransplantBridge extends Bifrost {
                 if (200 < response.code() || response.code() > 299) {
                     throw new ApiException(response.code(), "Response code: " + response.code());
                 }
+                System.out.println("yeah nah im back");
                 List<TransplantDetails> transplantDetails = handler.decodeTransplantList(response);
+                System.out.println(transplantDetails.size());
                 for (TransplantDetails transplantDetail : transplantDetails) {
                     AppController.getInstance().addTransplant(transplantDetail);
+                    AppController.getInstance().getClinicianController().refreshTables();
                 }
             }
         });
