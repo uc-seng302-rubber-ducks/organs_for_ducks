6aa8852341ceedae2a8cddfd4e07d413c4748d52
James Regan <jre103@uclive.ac.nz>
2018-08-27 14:58:52 +1200

    Added indexes to the Enum's for easier compatibility with the database
    #story[1151]

--- commons/src/main/java/odms/commons/model/_enum/AppointmentStatus.java
+++ commons/src/main/java/odms/commons/model/_enum/AppointmentStatus.java
@@ -1,15 +1,17 @@
 package odms.commons.model._enum;
 
 public enum AppointmentStatus {
-    PENDING("Pending"),
-    ACCEPTED("Accepted"),
-    REJECTED("Rejected"),
-    CANCELED("Canceled");
+    PENDING("Pending", 1),
+    ACCEPTED("Accepted", 2),
+    REJECTED("Rejected", 3),
+    CANCELED("Canceled", 4);
 
     private final String name;
+    private final int dbValue;
 
-    AppointmentStatus(String s)  {
+    AppointmentStatus(String s, int i)  {
         name = s;
+        dbValue = i;
     }
 
     public boolean equalsName(String otherName) {
@@ -19,5 +21,9 @@ public enum AppointmentStatus {
     @Override
     public String toString() {
         return this.name;
+    }
+
+    public int getDbValue() {
+        return this.dbValue;
     }
 }
