0378825a47db7a861bc0e4b36db8e5f1ecb129de
Jennifer Halvorsen <jha236@uclive.ac.nz>
2018-08-28 13:23:50 +1200

    Created the fxml file for the user to view their own appointments
    #story[1151]

---
+++ client/src/main/java/odms/controller/gui/panel/view/UserAppointmentViewController.java
@@ -0,0 +1,11 @@
+package odms.controller.gui.panel.view;
+
+import javafx.fxml.FXML;
+
+public class UserAppointmentViewController {
+
+    @FXML
+    public void openAppointmentPicker() {
+
+    }
+}
