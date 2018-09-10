9624d9c6b90b9815cff5e31ddc4646f67e0cfa7a
James Regan <jre103@uclive.ac.nz>
2018-09-10 22:06:46 +1200

    Added javadoc to getClinicianAppointments method in clinician bridge. Removed two empty methods that have been implemented in other branches
    #story[1191]

--- client/src/main/java/odms/bridge/AppointmentsBridge.java
+++ client/src/main/java/odms/bridge/AppointmentsBridge.java
@@ -94,6 +94,15 @@ public class AppointmentsBridge extends Bifrost {
         });
     }
 
+    /**
+     * Gets all the appointments for a specified clinician
+     *
+     * @param startIndex             Index to get results from
+     * @param count                  Amount of results to get at once
+     * @param staffId                Id of the staff to get appointments for
+     * @param token                  Authentication required to get the appointments.
+     * @param observableAppointments List to update with the gotten appointments
+     */
     public void getClinicianAppointments(int startIndex, int count, String staffId, String token, ObservableList<Appointment> observableAppointments) {
         String url = ip  + "/clinicians/" + staffId + APPOINTMENTS + "/" + "?startIndex=" + startIndex + "&count=" + count;
         Request request = new Request.Builder().addHeader(tokenHeader, token).url(url).build();
