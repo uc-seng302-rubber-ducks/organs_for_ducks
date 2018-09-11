e84add6a53d3f090df0fd20ccbe2713a89ce6dc6
Aaron Bong <acb116@uclive.ac.nz>
2018-09-11 20:11:22 +1200

    Implemented a calendar view to display calendars based on appointment categories. each calendar now have different colours.
    -updated pom file.
    
    #story[1152]

---
+++ client/src/main/java/odms/controller/gui/widget/CalendarWidget.java
@@ -0,0 +1,51 @@
+package odms.controller.gui.widget;
+
+import com.calendarfx.model.Calendar;
+import com.calendarfx.model.CalendarSource;
+import com.calendarfx.view.CalendarView;
+import odms.commons.model._enum.AppointmentCategory;
+
+import java.time.LocalTime;
+
+/**
+ * class that holds the configurations of
+ * the calendar.
+ */
+public class CalendarWidget {
+
+    /**
+     * creates a calendar based on the calendar configurations
+     *
+     * @return a calendar object
+     */
+    public static CalendarView createCalendar() {
+        CalendarView calendarView = new CalendarView();
+        calendarView.getCalendarSources().clear(); //removes default calendar
+        calendarView.setHeader(null);
+        calendarView.setFooter(null);
+        calendarView.setShowAddCalendarButton(false);
+
+        CalendarSource appointmentCategories = new CalendarSource("Appointment Categories");
+
+        Calendar bloodTestCalendar = new Calendar(AppointmentCategory.BLOOD_TEST.toString());
+        bloodTestCalendar.setStyle(Calendar.Style.STYLE1);
+
+        Calendar generalCheckUpCalendar = new Calendar(AppointmentCategory.GENERAL_CHECK_UP.toString());
+        generalCheckUpCalendar.setStyle(Calendar.Style.STYLE2);
+
+        Calendar healthAdviceCalendar = new Calendar(AppointmentCategory.HEALTH_ADVICE.toString());
+        healthAdviceCalendar.setStyle(Calendar.Style.STYLE3);
+
+        Calendar prescriptionCalendar = new Calendar(AppointmentCategory.PRESCRIPTION_RENEWAL.toString());
+        prescriptionCalendar.setStyle(Calendar.Style.STYLE4);
+
+        Calendar otherCalendar = new Calendar(AppointmentCategory.OTHER.toString());
+        otherCalendar.setStyle(Calendar.Style.STYLE5);
+
+        appointmentCategories.getCalendars().addAll(bloodTestCalendar, generalCheckUpCalendar, healthAdviceCalendar, prescriptionCalendar, otherCalendar);
+        calendarView.getCalendarSources().add(appointmentCategories);
+        calendarView.setRequestedTime(LocalTime.now());
+
+        return calendarView;
+    }
+}
