2cd47162b04d422e51e8d293656bb7ed749e7ebf
Jennifer Halvorsen <jha236@uclive.ac.nz>
2018-09-21 01:06:40 +1200

    Added an sql statement to retrieve a single blood test #story[1154]

--- server/src/main/java/odms/database/BloodTestHandler.java
+++ server/src/main/java/odms/database/BloodTestHandler.java
@@ -2,10 +2,7 @@ package odms.database;
 
 import odms.commons.model.datamodel.BloodTest;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.time.LocalDate;
 import java.util.Collection;
 
@@ -19,6 +16,8 @@ public class BloodTestHandler {
     private static final String UPDATE_BLOOD_TEST_STMT = "UPDATE BloodTests SET redBloodCellCount = ?, " +
             "whiteBloodCellCount = ?, haemoglobinLevel = ?, platelets = ?, glucoseLevels = ?, meanCellVolume = ?, " +
             "haematocrit = ?, meanCellHaematocrit = ?, resultsReceived = ?, requestedByClinician = ? WHERE bloodTestId = ?";
+
+    private static final String SELECT_ONE_BLOOD_TEST = "SELECT * FROM BloodTests WHERE bloodTestId = ?";
 
     /**
      * Saves and stores the given blood test within the database
@@ -57,7 +56,7 @@ public class BloodTestHandler {
      * @param bloodTest  The given blood test to be updated
      * @throws SQLException If there is an error updating the blood test or the connection is invalid
      */
-    public void putBloodTest(Connection connection, BloodTest bloodTest) throws SQLException {
+    public void putBloodTest(Connection connection, String nhi, BloodTest bloodTest) throws SQLException {
         try (PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_BLOOD_TEST_STMT)) {
             preparedStatement.setDouble(1, bloodTest.getRedBloodCellCount());
             preparedStatement.setDouble(2, bloodTest.getWhiteBloodCellCount());
@@ -84,9 +83,26 @@ public class BloodTestHandler {
         return null;
     }
 
-    public BloodTest getBloodTest(Connection connection, String nhi, int id) {
-        //Todo impement me :)
-        return null;
+    /**
+     * Retrieves a single blood test for the given user from the database
+     *
+     * @param connection Connection to the target database
+     * @param nhi        Unique identifier of the current user
+     * @param id         Unique identifier of the blood test
+     * @return           A blood test with values retrieved from the database
+     * @throws SQLException if the connection is invalid or there is an error with the database
+     */
+    public BloodTest getBloodTest(Connection connection, String nhi, int id) throws SQLException {
+        BloodTest bloodTest = null;
+        try (PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ONE_BLOOD_TEST)) {
+            preparedStatement.setInt(1, id);
+            try (ResultSet results = preparedStatement.executeQuery()) {
+                while (results.next()) {
+                    bloodTest = decodeBloodTestFromResultSet(results);
+                }
+            }
+        }
+        return bloodTest;
     }
 
     public Collection<BloodTest> getBloodTests(Connection connection, String nhi, LocalDate startDate, LocalDate endDate) {
@@ -94,4 +110,28 @@ public class BloodTestHandler {
         return null;
     }
 
+    /**
+     * Adds all the values of a result set to a blood test object and returns it to the user.
+     *
+     * @param resultSet result set containing values to generate the blood test
+     * @return  The blood test to be given back to the user
+     * @throws SQLException if there was an error with the result set
+     */
+    private BloodTest decodeBloodTestFromResultSet(ResultSet resultSet) throws SQLException {
+        BloodTest bloodTest = new BloodTest();
+        bloodTest.setBloodTestId(resultSet.getInt("bloodTestId"));
+        bloodTest.setRedBloodCellCount(resultSet.getDouble("redBloodCellCount"));
+        bloodTest.setWhiteBloodCellCount(resultSet.getDouble("whiteBloodCellCount"));
+        bloodTest.setHaemoglobinLevel(resultSet.getDouble("haemoglobinLevel"));
+        bloodTest.setPlatelets(resultSet.getDouble("platelets"));
+        bloodTest.setGlucoseLevels(resultSet.getDouble("glucoseLevels"));
+        bloodTest.setMeanCellVolume(resultSet.getDouble("meanCellVolume"));
+        bloodTest.setHaematocrit(resultSet.getDouble("haematocrit"));
+        bloodTest.setMeanCellHaematocrit(resultSet.getDouble("meanCellHaematocrit"));
+        bloodTest.setRequestedDate(resultSet.getDate("requestedDate").toLocalDate());
+        bloodTest.setResultsRecived(resultSet.getDate("resultsReceived").toLocalDate());
+        bloodTest.setRequestedByClinician(resultSet.getString("requestedByClinician"));
+
+        return bloodTest;
+    }
 }
