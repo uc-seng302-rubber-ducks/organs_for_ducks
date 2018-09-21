121805234024f275f391e9b0dbddab39a05b31f2
Jennifer Halvorsen <jha236@uclive.ac.nz>
2018-09-21 17:35:30 +1200

    Added tests for the blood test endpoints
    #story[1154]

--- server/src/test/java/odms/controller/BloodTestControllerTest.java
+++ server/src/test/java/odms/controller/BloodTestControllerTest.java
@@ -1,13 +1,18 @@
 package odms.controller;
 
+import odms.commons.model.datamodel.BloodTest;
 import odms.database.BloodTestHandler;
 import odms.database.DBHandler;
 import odms.database.JDBCDriver;
 import odms.exception.BadRequestException;
+import odms.exception.ServerDBException;
 import odms.socket.SocketHandler;
 import odms.utils.DBManager;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -27,6 +32,7 @@ public class BloodTestControllerTest {
     private SocketHandler socketHandler;
     private BloodTestController controller;
     private BloodTestHandler bloodTestHandler;
+    private BloodTest testBloodTest;
 
     @Before
     public void setUp() throws SQLException {
@@ -42,6 +48,7 @@ public class BloodTestControllerTest {
         when(manager.getDriver()).thenReturn(driver);
         when(bloodTestHandler.getBloodTests(any(Connection.class), anyString(), any(LocalDate.class), any(LocalDate.class), anyInt(), anyInt())).thenReturn(new ArrayList<>());
         controller = new BloodTestController(manager, socketHandler);
+        testBloodTest = new BloodTest();
     }
 
     @Test(expected = BadRequestException.class)
@@ -50,18 +57,70 @@ public class BloodTestControllerTest {
         verify(bloodTestHandler, times(0)).getBloodTests(any(Connection.class), anyString(), any(LocalDate.class), any(LocalDate.class), anyInt(), anyInt());
     }
 
-
     @Test(expected = BadRequestException.class)
     public void incorrectStartDateShouldNotParse() throws SQLException {
         controller.getBloodTests("a", "1999/01/01", "01/01/1999", 30, 0);
         verify(bloodTestHandler, times(0)).getBloodTests(any(Connection.class), anyString(), any(LocalDate.class), any(LocalDate.class), anyInt(), anyInt());
     }
 
-
     @Test
     public void correctDateShouldParse() throws SQLException {
         controller.getBloodTests("a", "01/01/1999", "01/02/1999", 30, 0);
         verify(bloodTestHandler, times(1)).getBloodTests(any(Connection.class), anyString(), any(LocalDate.class), any(LocalDate.class), anyInt(), anyInt());
+    }
+
+    @Test(expected = ServerDBException.class)
+    public void testGetAllBloodTestsThrowsException() throws SQLException {
+        when(driver.getConnection()).thenThrow(new SQLException());
+        controller.getBloodTests("ABC1234", "01/01/1999", "01/02/1999", 30, 0);
+    }
+
+    @Test
+    public void testGetSingleBloodTestReturnsBloodTest() throws SQLException {
+        controller.getBloodTest("ABC1234", 1);
+        verify(bloodTestHandler, times(1)).getBloodTest(any(Connection.class), anyString(), anyInt());
+    }
+
+    @Test(expected = ServerDBException.class)
+    public void testGetSingleBloodTestThrowsException() throws SQLException {
+        when(driver.getConnection()).thenThrow(new SQLException());
+        controller.getBloodTest("ABC1234", 1);
+    }
+
+    @Test
+    public void testPostBloodTestReturnsCreatedResponse() {
+        ResponseEntity response = controller.postBloodTest("ABC1234", testBloodTest);
+        Assert.assertEquals(HttpStatus.CREATED, response.getStatusCode());
+    }
+
+    @Test(expected = ServerDBException.class)
+    public void testPostBloodTestThrowsException() throws SQLException {
+        when(driver.getConnection()).thenThrow(new SQLException());
+        controller.postBloodTest("ABC1234", testBloodTest);
+    }
+
+    @Test
+    public void testDeleteBloodTestReturnsOkResponse() {
+        ResponseEntity response = controller.deleteBloodTest("ABC1234", 1);
+        Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test(expected = ServerDBException.class)
+    public void testDeleteBloodTestThrowsException() throws SQLException {
+        when(driver.getConnection()).thenThrow(new SQLException());
+        controller.deleteBloodTest("ABC1234", 1);
+    }
+
+    @Test
+    public void testPatchBloodTestReturnsOkResponse() {
+        ResponseEntity response = controller.patchBloodTest("ABC1234", 1, testBloodTest);
+        Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test(expected = ServerDBException.class)
+    public void testPatchBloodTestThrowsException() throws SQLException {
+        when(driver.getConnection()).thenThrow(new SQLException());
+        controller.patchBloodTest("ABC1234", 1, testBloodTest);
     }
 
 }
