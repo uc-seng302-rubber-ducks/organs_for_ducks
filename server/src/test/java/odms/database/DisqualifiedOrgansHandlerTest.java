641b92cc74aec1e34debafc85feb87a4c813f9ba
James Regan <jre103@uclive.ac.nz>
2018-09-22 12:07:38 +1200

    Added boolean field to disqualified organs ot mirror database and hold information about whether the disqualification should be deleted. Added updating and deleting to the function that handles updating the disqualified organs. Added update statements for disqualified organs and added if statements around the post and delete statements so they only activate on the correct organs. Added tests for insert and update sql methods and added a few more to delete to encompass new if statement
    #story[1158]

--- server/src/test/java/odms/database/DisqualifiedOrgansHandlerTest.java
+++ server/src/test/java/odms/database/DisqualifiedOrgansHandlerTest.java
@@ -50,6 +50,7 @@ public class DisqualifiedOrgansHandlerTest {
     private OrgansWithDisqualification createTestDisqualifiedOrgan(Integer id) {
         OrgansWithDisqualification testOrgan = new OrgansWithDisqualification(Organs.LIVER, "Testing", LocalDate.now(),"0");
         testOrgan.setDisqualifiedId(id);
+        testOrgan.setEligibleDate(LocalDate.now().plusDays(1));
         return testOrgan;
     }
 
@@ -91,6 +92,76 @@ public class DisqualifiedOrgansHandlerTest {
     }
 
     @Test
+    public void testPostDisqualifiedOrgans_ExecutesOnce_WithNullId() throws SQLException {
+        OrgansWithDisqualification testOrgan = createTestDisqualifiedOrgan(null);
+        Collection<OrgansWithDisqualification> disqualifications = new ArrayList<>();
+        disqualifications.add(testOrgan);
+
+        handler.postDisqualifiedOrgan(connection, disqualifications, "ABC1234");
+        verify(mockStmt, times(1)).executeUpdate();
+    }
+
+    @Test
+    public void testPostDisqualifiedOrgans_DoesNotExecute_WithNotNullId() throws SQLException {
+        OrgansWithDisqualification testOrgan = createTestDisqualifiedOrgan(0);
+        Collection<OrgansWithDisqualification> disqualifications = new ArrayList<>();
+        disqualifications.add(testOrgan);
+
+        handler.postDisqualifiedOrgan(connection, disqualifications, "ABC1234");
+        verify(mockStmt, times(0)).executeUpdate();
+    }
+
+    @Test
+    public void testPostDisqualifiedOrgans_ExecutesMoreThanOnce_WithOneNotNullId() throws SQLException {
+        OrgansWithDisqualification testOrgan1 = createTestDisqualifiedOrgan(null);
+        OrgansWithDisqualification testOrgan2 = createTestDisqualifiedOrgan(2);
+        OrgansWithDisqualification testOrgan3 = createTestDisqualifiedOrgan(null);
+
+        Collection<OrgansWithDisqualification> disqualifications = new ArrayList<>();
+        disqualifications.add(testOrgan1);
+        disqualifications.add(testOrgan2);
+        disqualifications.add(testOrgan3);
+
+        handler.postDisqualifiedOrgan(connection, disqualifications, "ABC1234");
+        verify(mockStmt, times(2)).executeUpdate();
+    }
+
+    @Test
+    public void testUpdateDisqualifiedOrgans_ExecutesOnce_WithNotNullId() throws SQLException {
+        OrgansWithDisqualification testOrgan = createTestDisqualifiedOrgan(0);
+        Collection<OrgansWithDisqualification> disqualifications = new ArrayList<>();
+        disqualifications.add(testOrgan);
+
+        handler.updateDisqualifiedOrgan(connection, disqualifications);
+        verify(mockStmt, times(1)).executeUpdate();
+    }
+
+    @Test
+    public void testUpdateDisqualifiedOrgans_DoesNotExecute_WithNullId() throws SQLException {
+        OrgansWithDisqualification testOrgan = createTestDisqualifiedOrgan(null);
+        Collection<OrgansWithDisqualification> disqualifications = new ArrayList<>();
+        disqualifications.add(testOrgan);
+
+        handler.updateDisqualifiedOrgan(connection, disqualifications);
+        verify(mockStmt, times(0)).executeUpdate();
+    }
+
+    @Test
+    public void testUpdateDisqualifiedOrgans_ExecutesMoreThanOnce_WithOneNullId() throws SQLException {
+        OrgansWithDisqualification testOrgan1 = createTestDisqualifiedOrgan(1);
+        OrgansWithDisqualification testOrgan2 = createTestDisqualifiedOrgan(null);
+        OrgansWithDisqualification testOrgan3 = createTestDisqualifiedOrgan(3);
+
+        Collection<OrgansWithDisqualification> disqualifications = new ArrayList<>();
+        disqualifications.add(testOrgan1);
+        disqualifications.add(testOrgan2);
+        disqualifications.add(testOrgan3);
+
+        handler.updateDisqualifiedOrgan(connection, disqualifications);
+        verify(mockStmt, times(2)).executeUpdate();
+    }
+
+    @Test
     public void testDeleteDisqualifiedOrgans_DoesNotCommit_NullId() throws SQLException {
         OrgansWithDisqualification testOrgan = createTestDisqualifiedOrgan(null);
         Collection<OrgansWithDisqualification> disqualifications = new ArrayList<>();
@@ -101,8 +172,9 @@ public class DisqualifiedOrgansHandlerTest {
     }
 
     @Test
-    public void testDeleteDisqualifiedOrgans_CommitsOnce() throws SQLException {
+    public void testDeleteDisqualifiedOrgans_CommitsOnce_WithNotCurrentlyDisqualifiedOrgan() throws SQLException {
         OrgansWithDisqualification testOrgan = createTestDisqualifiedOrgan(0);
+        testOrgan.setCurrentlyDisqualified(false);
         Collection<OrgansWithDisqualification> disqualifications = new ArrayList<>();
         disqualifications.add(testOrgan);
 
@@ -111,10 +183,23 @@ public class DisqualifiedOrgansHandlerTest {
     }
 
     @Test
+    public void testDeleteDisqualifiedOrgans_DoesNotCommit_WithCurrentlyDisqualifiedOrgan() throws SQLException {
+        OrgansWithDisqualification testOrgan = createTestDisqualifiedOrgan(0);
+        Collection<OrgansWithDisqualification> disqualifications = new ArrayList<>();
+        disqualifications.add(testOrgan);
+
+        handler.deleteDisqualifiedOrgan(connection, disqualifications);
+        verify(connection, times(0)).commit();
+    }
+
+    @Test
     public void testDeleteDisqualifiedOrgans_CommitsMoreThanOnce() throws SQLException {
         OrgansWithDisqualification testOrgan1 = createTestDisqualifiedOrgan(1);
         OrgansWithDisqualification testOrgan2 = createTestDisqualifiedOrgan(2);
         OrgansWithDisqualification testOrgan3 = createTestDisqualifiedOrgan(3);
+        testOrgan1.setCurrentlyDisqualified(false);
+        testOrgan2.setCurrentlyDisqualified(false);
+        testOrgan3.setCurrentlyDisqualified(false);
 
         Collection<OrgansWithDisqualification> disqualifications = new ArrayList<>();
         disqualifications.add(testOrgan1);
@@ -126,10 +211,31 @@ public class DisqualifiedOrgansHandlerTest {
     }
 
     @Test
+    public void testDeleteDisqualifiedOrgans_CommitsMoreThanOnce_WhenOneOrganIsStillDisqualified() throws SQLException {
+        OrgansWithDisqualification testOrgan1 = createTestDisqualifiedOrgan(1);
+        OrgansWithDisqualification testOrgan2 = createTestDisqualifiedOrgan(2);
+        OrgansWithDisqualification testOrgan3 = createTestDisqualifiedOrgan(3);
+        testOrgan1.setCurrentlyDisqualified(false);
+        testOrgan2.setCurrentlyDisqualified(true);
+        testOrgan3.setCurrentlyDisqualified(false);
+
+        Collection<OrgansWithDisqualification> disqualifications = new ArrayList<>();
+        disqualifications.add(testOrgan1);
+        disqualifications.add(testOrgan2);
+        disqualifications.add(testOrgan3);
+
+        handler.deleteDisqualifiedOrgan(connection, disqualifications);
+        verify(connection, times(2)).commit();
+    }
+
+    @Test
     public void testDeleteDisqualifiedOrgans_CommitsMoreThanOnce_WhenOneIdIsNull() throws SQLException {
         OrgansWithDisqualification testOrgan4 = createTestDisqualifiedOrgan(4);
         OrgansWithDisqualification testOrgan5 = createTestDisqualifiedOrgan(null);
         OrgansWithDisqualification testOrgan6 = createTestDisqualifiedOrgan(6);
+        testOrgan4.setCurrentlyDisqualified(false);
+        testOrgan5.setCurrentlyDisqualified(false);
+        testOrgan6.setCurrentlyDisqualified(false);
 
         Collection<OrgansWithDisqualification> disqualifications = new ArrayList<>();
         disqualifications.add(testOrgan4);
@@ -143,6 +249,7 @@ public class DisqualifiedOrgansHandlerTest {
     @Test
     public void testDeleteDisqualifiedOrgans_RollsBack_WhenSqlException() throws SQLException {
         OrgansWithDisqualification testOrgan = createTestDisqualifiedOrgan(0);
+        testOrgan.setCurrentlyDisqualified(false);
 
         Collection<OrgansWithDisqualification> disqualifications = new ArrayList<>();
         disqualifications.add(testOrgan);
