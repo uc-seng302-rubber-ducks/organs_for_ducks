78214e66b78df79fb078ff5975b8711e6b461452
Eiran Ling <eli26@uclive.ac.nz>
2018-09-25 13:13:02 +1200

    Javadoc

--- client/src/main/java/odms/controller/gui/widget/LoadingTableView.java
+++ client/src/main/java/odms/controller/gui/widget/LoadingTableView.java
@@ -4,7 +4,13 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TableView;
 
+/**
+ * An extension of a table view to allow the capability of showing when it is loading data.
+ */
 public class LoadingTableView<T> extends TableView<T> implements LoadingWidget {
+    /**
+     * Sets the placeholder of the tableview by telling it whether or not it is waiting for incoming data
+     */
     public void setWaiting(boolean waiting) {
         if (waiting) {
             ProgressIndicator indicator = new ProgressIndicator(ProgressIndicator.INDETERMINATE_PROGRESS);
